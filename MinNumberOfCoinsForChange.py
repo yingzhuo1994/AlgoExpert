f02a2d83406df8ab248075d012008d60f3a083e7
Ying Zhuo <zhuoying1994@gmail.com>
2021-05-10 15:37:44 -0500

    Simplify the solution.

--- MinNumberOfCoinsForChange.py
+++ MinNumberOfCoinsForChange.py
@@ -1,13 +1,11 @@
 def minNumberOfCoinsForChange(n, denoms):
-    # Write your code here.
-    lst = [-1 for _ in range(n + 1)]
+    # O(nd) time | O(n) space
+    lst = [float('inf') for _ in range(n + 1)]
 	lst[0] = 0
 	denoms.sort()
     for denom in denoms:
         for i in range(len(lst)):
-            if lst[i] >= 0:
-                lst[i] = min(lst[i], 1 + lst[i - denom])
-            else:
-                lst[i] = 1 + lst[i - denom]
-		print(denom, lst)
-    return lst[-1]
+            if denom <= i and lst[i - denom] >= 0:
+				lst[i] = min(lst[i], 1 + lst[i - denom])
+    return lst[-1] if lst[-1] != float('inf') else - 1
+
