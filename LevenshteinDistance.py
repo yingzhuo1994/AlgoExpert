2214afa64ff439773ab991aa2f2b7e21cbb3a647
Ying Zhuo <zhuoying1994@gmail.com>
2021-05-11 23:05:37 -0500

    2nd dynamic solution

--- LevenshteinDistance.py
+++ LevenshteinDistance.py
@@ -1,6 +1,9 @@
+from typing import Counter
+
+
 def levenshteinDistance(str1, str2):
     # Write your code here.
-    print(str1, str2)
+    # 1st recursive solution
     if str1 == str2:
         return 0
     if len(str1) == 0:
@@ -15,4 +18,18 @@ def levenshteinDistance(str1, str2):
     print(insertOpt, deletionOpt, substitutionOpt)
     return min(insertOpt, deletionOpt, substitutionOpt)
 
-print(levenshteinDistance("abcdefghij", "1234567890"))
+    # 2nd dynamic solution
+    table = [[0 for j in range(len(str2) + 1)] for i in range(len(str1) + 1)]
+    for j in range(1, len(str2) + 1):
+        table[0][j] = j
+    for i in range(1, len(str1) + 1):
+        table[i][0] = i
+    for i in range(1, len(str1) + 1):
+        for j in range(1, len(str2) + 1):
+            cur = min(table[i-1][j], table[i][j-1], table[i-1][j-1])
+            if str1[i-1] == str2[j-1]:
+                table[i][j] = table[i - 1][j - 1]
+            else:
+                table[i][j] = cur + 1
+    return table[-1][-1]
+
