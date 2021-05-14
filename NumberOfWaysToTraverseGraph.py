1580665772e70198f52d442984e508d888750fe8
Ying Zhuo <zhuoying1994@gmail.com>
2021-05-13 20:00:16 -0500

    Add 2nd solution

--- NumberOfWaysToTraverseGraph.py
+++ NumberOfWaysToTraverseGraph.py
@@ -1,5 +1,6 @@
 def numberOfWaysToTraverseGraph(width, height):
     # Write your code here.
+    # 1st solution
     # O(mn) time | O(n) space
     steps = [1 for _ in range(width)]
     for i in range(height - 1):
@@ -7,3 +8,14 @@ def numberOfWaysToTraverseGraph(width, height):
             steps[j] = steps[j] + steps[j + 1]
     return steps[0]
 
+    # 2nd solution
+    # O(m + n) time | O(1) space
+    def factorial(n):
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+    
+    m = width - 1
+    n = height - 1
+    return factorial(m + n) // (factorial(m) * factorial(n))
