78e27503b2a5d3a4e9456e2f5d7b7881558ba1ad
Ying Zhuo <zhuoying1994@gmail.com>
2021-05-13 19:05:49 -0500

    Solution

---
+++ KadanesAlgorithm.py
@@ -0,0 +1,9 @@
+def kadanesAlgorithm(array):
+    # Write your code here.
+    maxEndingHere = array[0]
+    maxSoFar = array[0]
+    for i in range(1, len(array)):
+        num = array[i]
+        maxEndingHere = max(num, maxEndingHere + num)
+        maxSoFar = max(maxSoFar, maxEndingHere)
+    return maxSoFar
