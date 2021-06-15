53be372c5f31ee3724f79f991320a729f4921558
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-06-14 22:02:15 -0500

    Add the 2nd solution

--- StaircaseTraversal.py
+++ StaircaseTraversal.py
@@ -8,3 +8,20 @@ def staircaseTraversal(height, maxSteps):
 			if i - k >= 0:
 				lst[i] += lst[i -k]
 	return lst[-1]
+
+# 2nd solution
+# O(n) time | O(n) space
+def staircaseTraversal(height, maxSteps):
+	currentNumberOfWays = 0
+	waysToTop = [1]
+
+	for currentHeight in range(1, height + 1):
+		startOfWindow = currentHeight - maxSteps - 1
+		endOfWindow = currentHeight - 1
+		if startOfWindow >= 0:
+			currentNumberOfWays -= waysToTop[startOfWindow]
+		
+		currentNumberOfWays += waysToTop[endOfWindow]
+		waysToTop.append(currentNumberOfWays)
+	
+	return waysToTop[height]
