48abe7919a41defbc64e6b1ddce011f9a3e1d636
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-06-26 18:15:15 -0500

    1st solution

---
+++ NextGreaterElement.py
@@ -0,0 +1,15 @@
+def nextGreaterElement(array):
+    # Write your code here.
+    newArray = []
+    for i in range(len(array)):
+        newArray.append(getNextGreaterElement(array, i))
+    return newArray
+
+def getNextGreaterElement(array, idx):
+    for i in range(idx + 1, len(array)):
+        if array[i] > array[idx]:
+            return array[i]
+    for i in range(idx):
+        if array[i] > array[idx]:
+            return array[i]
+    return -1
