3cebda1f36d9418697b08c807c2930aa7bfbc115
Ying Zhuo <zhuoying1994@gmail.com>
2021-06-27 16:27:00 -0500

    Solution

--- NextGreaterElement.py
+++ NextGreaterElement.py
@@ -1,11 +1,18 @@
+<<<<<<< HEAD
 # 1st brute-force solution
 # O(n^2) time | O(n) space
+=======
+>>>>>>> 48abe7919a41defbc64e6b1ddce011f9a3e1d636
 def nextGreaterElement(array):
     # Write your code here.
     newArray = []
     for i in range(len(array)):
         newArray.append(getNextGreaterElement(array, i))
+<<<<<<< HEAD
 	return newArray
+=======
+    return newArray
+>>>>>>> 48abe7919a41defbc64e6b1ddce011f9a3e1d636
 
 def getNextGreaterElement(array, idx):
     for i in range(idx + 1, len(array)):
@@ -14,4 +21,8 @@ def getNextGreaterElement(array, idx):
     for i in range(idx):
         if array[i] > array[idx]:
             return array[i]
+<<<<<<< HEAD
     return -1
+=======
+    return -1
+>>>>>>> 48abe7919a41defbc64e6b1ddce011f9a3e1d636
