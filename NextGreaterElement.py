f496bcafcc8e3f219289a97d2051e52b91148d47
Ying Zhuo <zhuoying1994@gmail.com>
2021-06-27 16:35:42 -0500

    Add two solutions

--- NextGreaterElement.py
+++ NextGreaterElement.py
@@ -1,5 +1,6 @@
+# 1st brute-force solution
+# O(n^2) time | O(n) space
 def nextGreaterElement(array):
-    # Write your code here.
     newArray = []
     for i in range(len(array)):
         newArray.append(getNextGreaterElement(array, i))
@@ -13,3 +14,40 @@ def getNextGreaterElement(array, idx):
         if array[i] > array[idx]:
             return array[i]
     return -1
+
+# 2nd solution
+# O(n) time | O(n) space
+def nextGreaterElement(array):
+    result = [-1] * len(array)
+    stack = []
+
+    for idx in range(2 * len(array)):
+        circularIdx = idx % len(array)
+
+        while len(stack) > 0 and array[stack[-1]] < array[circularIdx]:
+            top = stack.pop()
+            result[top] = array[circularIdx]
+        
+        stack.append(circularIdx)
+
+    return result
+
+# 3rd solution
+# O(n) time | O(n) space
+def nextGreaterElement(array):
+    result = [-1] * len(array)
+    stack = []
+
+    for idx in range(2 * len(array) - 1, -1, -1):
+        circularIdx = idx % len(array)
+
+        while len(stack) > 0:
+            if stack[len(stack) - 1] <= array[circularIdx]:
+                stack.pop()
+            else:
+                result[circularIdx] = stack[len(stack) - 1]
+                break
+        
+        stack.append(array[circularIdx])
+
+    return result
