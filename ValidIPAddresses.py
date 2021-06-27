0469b14e20a0ea944071ea00eeb65f4ad437db8e
Ying Zhuo <zhuoying1994@gmail.com>
2021-06-27 17:10:31 -0500

    Add the 2nd solution

--- ValidIPAddresses.py
+++ ValidIPAddresses.py
@@ -1,3 +1,4 @@
+# 1st solution
 def validIPAddresses(string):
     # Write your code here.
     lst = []
@@ -19,3 +20,35 @@ def check(s):
     if num > 255:
         return False
     return len(s) == len(str(num))
+
+# 2nd solution
+# O(1) time | O(1) space
+def validIPAddresses(string):
+    ipAddressesFound = []
+
+    for i in range(1, min(len(string), 4)):
+        currentIPAddressParts = ["", "", "", ""]
+
+        currentIPAddressParts[0] = string[:i]
+        if not isValidPart(currentIPAddressParts[0]):
+            continue
+
+        for j in range(i + 1, i + min(len(string) - i, 4)):
+            currentIPAddressParts[1] = string[i:j]
+            if not isValidPart(currentIPAddressParts[1]):
+                continue
+
+            for k in range(j + 1, j + min(len(string) - j, 4)):
+                currentIPAddressParts[2] = string[j:k]
+                currentIPAddressParts[3] = string[k:]
+                if isValidPart(currentIPAddressParts[2]) and isValidPart(currentIPAddressParts[3]):
+                    ipAddressesFound.append(".".join(currentIPAddressParts))
+        
+    return ipAddressesFound
+
+def isValidPart(string):
+    stringAsInt = int(string)
+    if stringAsInt > 255:
+        return False
+    
+    return len(string) == len(str(stringAsInt))
