59321277962bdd0303f2602a4ef6e36e4ecefefa
Ying Zhuo <zhuoying1994@gmail.com>
2021-06-30 15:05:25 -0500

    Add the 2nd solution

--- ReverseWordsInString.py
+++ ReverseWordsInString.py
@@ -1,3 +1,4 @@
+# 1st solution
 # O(n) time | O(n) space
 def reverseWordsInString(string):
     # Write your code here.
@@ -16,3 +17,26 @@ def reverseWordsInString(string):
     words.append(string[startOfWord:])
 
     return ''.join(words[::-1])
+
+# 2nd soluion
+# O(n) time | O(n) space
+def reverseWordsInString(string):
+    characters = [char for char in string]
+    reverseListRange(characters, 0, len(characters) - 1)
+
+    startOfWord = 0
+    while startOfWord < len(characters):
+        endOfWord = startOfWord
+        while endOfWord < len(characters) and characters[endOfWord] != " ":
+            endOfWord += 1
+        
+        reverseListRange(characters, startOfWord, endOfWord - 1)
+        startOfWord = endOfWord + 1
+
+    return "".join(characters)
+
+def reverseListRange(list, start, end):
+    while start < end:
+        list[start], list[end] = list[end], list[start]
+        start += 1
+        end -= 1
