11cd7ef284e6e29b54fbc88d9abbd0440d1e2427
Ying Zhuo <zhuoying1994@gmail.com>
2021-07-04 08:59:20 -0500

    Add a solution

--- StaircaseTraversal.py
+++ StaircaseTraversal.py
@@ -11,6 +11,23 @@ def staircaseTraversal(height, maxSteps):
 	return lst[-1]
 
 # 2nd solution
+# O(n * k) time | O(n) space - where n is the height of the staircase and k is the number of allowed steps
+def staircaseTraversal(height, maxSteps):
+	return numberOfWaysToTop(height, maxSteps, {0: 1, 1: 1})
+
+def numberOfWaysToTop(height, maxSteps, memoize):
+	if height in memoize:
+		return memoize[height]
+	
+	numberOfWays = 0
+	for step in range(1, min(maxSteps, height) + 1):
+		numberOfWays += numberOfWaysToTop(height - step, maxSteps, memoize)
+	
+	memoize[height] = numberOfWays
+
+	return numberOfWays
+
+# 3rd solution
 # O(n) time | O(n) space
 def staircaseTraversal(height, maxSteps):
 	currentNumberOfWays = 0
