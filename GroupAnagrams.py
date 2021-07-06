2e0b6a5c22dfb2fd00dde710a88b7ba7d8bba857
Ying Zhuo <zhuoying1994@gmail.com>
2021-07-05 20:15:49 -0500

    Add a solution

--- GroupAnagrams.py
+++ GroupAnagrams.py
@@ -1,3 +1,37 @@
+# 1st solution
+# O(w * n * log(n) + n * w * log(w)) time | O(wn) space
+# where w is the number of words and n is the length of the longest word
+def groupAnagrams(words):
+	if len(words) == 0:
+		return []
+	
+	sortedWords = ["".join(sorted(w)) for w in words]
+	indices = [i for i in range(len(words))]
+	indices.sort(key = lambda x: sortedWords[x])
+
+	result = []
+	currentAnagramGroup = []
+	currentAnagram = sortedWords[indices[0]]
+	for index in indices:
+		word = words[index]
+		sortedWord = sortedWords[index]
+
+		if sortedWord == currentAnagram:
+			currentAnagramGroup.append(word)
+			continue
+
+		result.append(currentAnagramGroup)
+		currentAnagramGroup = [word]
+		currentAnagram = sortedWord
+	
+	result.append(currentAnagramGroup)
+
+	return result
+
+
+# 2nd solution
+# O(w * n * log(n)) time | O(wn) space
+# where w is the number of words and n is the length of the longest word
 def groupAnagrams(words):
     # Write your code here.
 	d = {}
