a8736df114246401dd9dca90f04818bf96048e89
Ying Zhuo <zhuoying1994@gmail.com>
2021-07-07 09:45:53 -0500

    Add the 3rd solution

--- MinRewards.py
+++ MinRewards.py
@@ -45,3 +45,15 @@ def expandFromLocalMinIdx(localMinIdx, scores, rewards):
     while rightIdx < len(scores) and scores[rightIdx] > scores[rightIdx - 1]:
         rewards[rightIdx] = rewards[rightIdx - 1] + 1
         rightIdx += 1
+
+# 3rd solution
+# O(n) time | O(n) space
+def minRewards(scores):
+    rewards = [1 for _ in scores]
+    for i in range(1, len(scores)):
+        if scores[i] > scores[i - 1]:
+            rewards[i] = rewards[i - 1] + 1
+    for i in reversed(range(len(scores) - 1)):
+        if scores[i] > scores[i + 1]:
+            rewards[i] = max(rewards[i], rewards[i + 1] + 1)
+    return sum(rewards)
