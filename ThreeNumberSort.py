7da36a091d59729c1f5b6e67a999aeedb2f0253e
Ying Zhuo <zhuoying1994@gmail.com>
2021-07-15 10:41:15 -0500

    Add the 3rd solution

--- ThreeNumberSort.py
+++ ThreeNumberSort.py
@@ -36,3 +36,23 @@ def threeNumberSort(array, order):
             array[currentIdx] = value
     
     return array
+
+# 3rd solution
+# O(n) time | O(1) space
+def threeNumberSort(array, order):
+    firstValue = order[0]
+    thirdValue = order[2]
+
+    firstIdx = 0
+    for idx in range(len(array)):
+        if array[idx] == firstValue:
+            array[firstIdx], array[idx] = array[idx], array[firstIdx]
+            firstIdx += 1
+    
+    thirdIdx = len(array) - 1
+    for idx in range(len(array) - 1, - 1, -1):
+        if array[idx] == thirdValue:
+            array[thirdIdx], array[idx] = array[idx], array[thirdIdx]
+            thirdIdx -= 1
+        
+    return array
