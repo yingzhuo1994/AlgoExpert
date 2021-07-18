a4431a3470719a1da9a0163314825cb09b75b421
Ying Zhuo <zhuoying1994@gmail.com>
2021-07-18 17:27:16 -0500

    Add the 2nd solution

--- ValidateThreeNodes.py
+++ ValidateThreeNodes.py
@@ -20,9 +20,28 @@ def validateThreeNodes(nodeOne, nodeTwo, nodeThree):
 def isDescendant(ancestor, descendant):
     if not ancestor:
         return False
-    if ancestor == descendant:
+    if ancestor is descendant:
         return True
     if descendant.value < ancestor.value:
         return isDescendant(ancestor.left, descendant)
     else:
         return isDescendant(ancestor.right, descendant)
+
+# 1st solution
+# O(h) time | O(1) space - where h is the height of the tree
+def validateThreeNodes(nodeOne, nodeTwo, nodeThree):
+    # Write your code here.
+    if isDescendant(nodeOne, nodeTwo):
+		return isDescendant(nodeTwo, nodeThree)
+    elif isDescendant(nodeThree, nodeTwo):
+		return isDescendant(nodeTwo, nodeOne)
+    else:
+        return False
+
+def isDescendant(ancestor, descendant):
+    while ancestor and ancestor is not descendant:
+        if descendant.value < ancestor.value:
+            ancestor = ancestor.left
+        else:
+            ancestor = ancestor.right
+    return descendant is ancestor
