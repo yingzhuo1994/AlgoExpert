7ae881f38c8d6dc2ff8a636060d261e7f83def4b
Ying Zhuo <zhuoying1994@gmail.com>
2021-07-19 10:58:21 -0500

    Add the 3rd solution

--- ValidateThreeNodes.py
+++ ValidateThreeNodes.py
@@ -27,7 +27,7 @@ def isDescendant(ancestor, descendant):
     else:
         return isDescendant(ancestor.right, descendant)
 
-# 1st solution
+# 2nd solution
 # O(h) time | O(1) space - where h is the height of the tree
 def validateThreeNodes(nodeOne, nodeTwo, nodeThree):
     # Write your code here.
@@ -45,3 +45,44 @@ def isDescendant(ancestor, descendant):
         else:
             ancestor = ancestor.right
     return descendant is ancestor
+
+# 3rd solution
+# O(d) time | O(1) space - where d is the distance between nodeOne and nodeThree
+def validateThreeNodes(nodeOne, nodeTwo, nodeThree):
+    searchOne = nodeOne
+    searchTwo = nodeThree
+
+    while True:
+        foundThreeFromOne = searchOne is nodeThree
+        foundOneFromThree = searchTwo is nodeOne
+        foundNodeTwo = searchOne is nodeTwo or searchTwo is nodeTwo
+        finishedSearching = searchOne is None and searchTwo is None
+        if foundThreeFromOne or foundOneFromThree or foundNodeTwo or finishedSearching:
+            break
+
+        if searchOne:
+            if searchOne.value > nodeTwo.value :
+                searchOne = searchOne.left
+            else:
+                searchOne = searchOne.right
+        
+        if searchTwo:
+            if searchTwo.value > nodeTwo.value:
+                searchTwo = searchTwo.left
+            else:
+                searchTwo = searchTwo.right
+        
+    foundNodeFromOther = searchOne is nodeThree or searchTwo is nodeOne
+    foundNodeTwo = searchOne is nodeTwo or searchTwo is nodeTwo
+    if not foundNodeTwo or foundNodeFromOther:
+        return False
+    
+    return searchForTarget(nodeTwo, nodeThree if searchOne is nodeTwo else nodeOne)
+
+def searchForTarget(node, target):
+    while node is not None and node is not target:
+        if target.value < node.value:
+            node = node.left
+        else:
+            node = node.right
+    return node is target
