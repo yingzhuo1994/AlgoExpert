83d1b7388388bfc1c95a74b02a728c7a7f3ad9a2
Ying Zhuo <zhuoying1994@gmail.com>
2021-07-26 21:30:41 -0500

    Add the 2nd solution

--- FindNodesDistanceK.py
+++ FindNodesDistanceK.py
@@ -6,6 +6,7 @@ class BinaryTree:
         self.right = right
 
 
+# 1st solution
 # O(n) time | O(n) space - where n is the number of nodes in the tree
 def findNodesDistanceK(tree, target, k):
     nodesToParents = {}
@@ -55,5 +56,43 @@ def populateNodesToParents(node, nodesToParents, parent = None):
         populateNodesToParents(node.left, nodesToParents, node)
         populateNodesToParents(node.right, nodesToParents, node)
 
-    
+# 2nd solution
+# O(n) time | O(n) space
+def findNodesDistanceK(tree, target, k):
+    nodesDistanceK = []
+    findDistanceFromNodeToTarget(tree, target, k, nodesDistanceK)
+    return nodesDistanceK
 
+def findDistanceFromNodeToTarget(node, target, k, nodesDistanceK):
+    if node is None:
+        return -1
+    
+    if node.value == target:
+        addSubtreeNodesAtDistanceK(node, 0, k, nodesDistanceK)
+        return 1
+    
+    leftDistance = findDistanceFromNodeToTarget(node.left, target, k, nodesDistanceK)
+    rightDistance = findDistanceFromNodeToTarget(node.right, target, k, nodesDistanceK)
+
+    if leftDistance == k or rightDistance == k:
+        nodesDistanceK.append(node.value)
+    
+    if leftDistance != -1:
+        addSubtreeNodesAtDistanceK(node.right, leftDistance + 1, k, nodesDistanceK)
+        return leftDistance + 1
+    
+    if rightDistance != -1:
+        addSubtreeNodesAtDistanceK(node.left, rightDistance + 1, k, nodesDistanceK)
+        return rightDistance + 1
+    
+    return -1
+
+def addSubtreeNodesAtDistanceK(node, distance, k, nodesDistanceK):
+    if node is None:
+        return 
+    
+    if distance == k:
+        nodesDistanceK.append(node.value)
+    else:
+        addSubtreeNodesAtDistanceK(node.left, distance + 1, k, nodesDistanceK)
+        addSubtreeNodesAtDistanceK(node.right, distance + 1, k, nodesDistanceK)
