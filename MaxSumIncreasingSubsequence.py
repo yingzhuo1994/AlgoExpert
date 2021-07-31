1f5d1c6deff387944df6779efea2e057ef4a1cfe
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-07-31 13:49:58 -0500

    Add the official solution.

--- MaxSumIncreasingSubsequence.py
+++ MaxSumIncreasingSubsequence.py
@@ -1,3 +1,4 @@
+# 1st solution, my solution
 # O(n^2) time | O(n) space
 def maxSumIncreasingSubsequence(array):
     # Write your code here.
@@ -22,3 +23,27 @@ def maxSumIncreasingSubsequence(array):
         lst.append(array[start])
         nextIdx = table[start][0]
     return [maxSum, lst[::-1]]
+
+# 2nd solution, official solution
+# O(n^2) time | O(n) space
+def maxSumIncreasingSubsequence(array):
+    sequences = [None for _ in array]
+    sums = [num for num in array]
+    maxSumIdx = 0
+    for i in range(len(array)):
+        currentNum = array[i]
+        for j in range(0, i):
+            otherNum = array[j]
+            if otherNum < currentNum and sums[j] + currentNum > sums[i]:
+                sums[i] = sums[j] + currentNum
+                sequences[i] = j
+        if sums[i] >= sums[maxSumIdx]:
+            maxSumIdx = i
+    return [sums[maxSumIdx], buildSequence(array, sequences, maxSumIdx)]
+
+def buildSequence(array, sequences, currentIdx):
+    sequence = []
+    while currentIdx is not None:
+        sequence.append(array[currentIdx])
+        currentIdx = sequences[currentIdx]
+    return list(reversed(sequence))
