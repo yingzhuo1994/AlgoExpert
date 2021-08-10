66300072417dce62fda92174ed3aac274a2318e7
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-08-10 10:31:23 -0500

    Add the 2nd solution

--- LongestCommonSubsequence.py
+++ LongestCommonSubsequence.py
@@ -9,3 +9,25 @@ def longestCommonSubsequence(str1, str2):
             else:
                 lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1], key = len)
     return lcs[-1][-1]
+
+# 2nd solution
+# O(nm*min(n, m)) time | O((min(n, m)^2)) space
+def longestCommonSubsequence(str1, str2):
+    small = str1 if len(str1) < len(str2) else str2
+    big = str1 if len(str1) >= len(str2) else str2
+    evenLcs = [[] for x in range(len(small) + 1)]
+    oddLcs = [[] for x in range(len(small) + 1)]
+    for i in range(1, len(big) + 1):
+        if i % 2 == 1:
+            currentLcs = oddLcs
+            previousLcs = evenLcs
+        else:
+            currentLcs = evenLcs
+            previousLcs = oddLcs
+        for j in range(1, len(small) + 1):
+            if big[i - 1] == small[j -1]:
+                currentLcs[j] = previousLcs[j - 1] + [big[i - 1]]
+            else:
+                currentLcs[j] = max(previousLcs[j], currentLcs[j - 1], key=len)
+    return evenLcs[-1] if len(big) % 2 == 0 else oddLcs[-1]
+    
