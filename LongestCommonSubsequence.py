41cabe870ddda9770c43767d21c68b3bbac7bb2a
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-08-14 10:43:15 -0500

    Add the 3rd solution

--- LongestCommonSubsequence.py
+++ LongestCommonSubsequence.py
@@ -30,4 +30,30 @@ def longestCommonSubsequence(str1, str2):
             else:
                 currentLcs[j] = max(previousLcs[j], currentLcs[j - 1], key=len)
     return evenLcs[-1] if len(big) % 2 == 0 else oddLcs[-1]
-    
+
+# 3rd solution
+# O(nm) time | O(nm) space
+def longestCommonSubsequence(str1, str2):
+    lcs = [[[None, 0, None, None] for x in range(len(str1) + 1)] for y in range(len(str2) + 1)]
+    for i in range(1, len(str2) + 1):
+        for j in range(1, len(str1) + 1):
+            if str2[i - 1] == str1[j - 1]:
+                lcs[i][j] = [str2[i - 1], lcs[i - 1][j - 1][1] + 1, i - 1, j - 1]
+            else:
+                if lcs[i - 1][j][1] > lcs[i][j - 1][1]:
+                    lcs[i][j] = [None, lcs[i - 1][j][1], i - 1, j]
+                else:
+                    lcs[i][j] = [None, lcs[i][j - 1][1], i, j - 1]
+    return buildSequence(lcs)
+
+def buildSequence(lcs):
+    sequence = []
+    i = len(lcs) - 1
+    j = len(lcs[0]) - 1
+    while i != 0 and j != 0:
+        currentEntry = lcs[i][j]
+        if currentEntry[0] is not None:
+            sequence.append(currentEntry[0])
+        i = currentEntry[2]
+        j = currentEntry[3]
+    return list(reversed(sequence))
