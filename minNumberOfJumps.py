214c47736e744f30b1b317ba759787c79f1dd4f9
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-08-15 23:37:30 -0500

    1st solution

--- minNumberOfJumps.py
+++ minNumberOfJumps.py
@@ -1,2 +1,8 @@
 def minNumberOfJumps(array):
-    
+    table = [float('inf') for _ in array]
+    table[0] = 0
+    for i in range(len(array)):
+        for j in range(1, array[i] + 1):
+            if i + j < len(array):
+                table[i + j] = min(table[i + j], table[i] + 1)
+    return table[-1]
