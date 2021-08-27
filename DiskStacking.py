fc5f28dfe8d47b9df85922972bc5ee5a50e919c1
yingzhuo <yingzhuo1994@gmail.com>
2021-08-27 09:46:11 -0500

    Official solution

--- DiskStacking.py
+++ DiskStacking.py
@@ -1,3 +1,30 @@
+# O(n^2) time | O(n) space
 def diskStacking(disks):
-    # Write your code here.
-    pass
+    disks.sort(key = lambda disk: disk[2])
+    heights = [disk[2] for disk in disks]
+    sequences = [None for disk in disks]
+    maxHeightIdx = 0
+    for i in range(1, len(disks)):
+        currentDisk = disks[i]
+        for j in range(0, i):
+            otherDisk = disks[j]
+            if areValidDimensions(otherDisk, currentDisk):
+                if heights[i] <= currentDisk[2] + heights[j]:
+                    heights[i] = currentDisk[2] + heights[j]
+                    sequences[i] = j
+        if heights[i] >= heights[maxHeightIdx]:
+            maxHeightIdx = i
+    return buildSequence(disks, sequences, maxHeightIdx)
+
+def areValidDimensions(o, c):
+    for i in range(len(o)):
+        if o[i] >= c[i]:
+            return False
+    return True
+
+def buildSequence(array, sequences, currentIdx):
+    sequence = []
+    while currentIdx is not None:
+        sequence.append(array[currentIdx])
+        currentIdx = sequences[currentIdx]
+    return list(reversed(sequence))
