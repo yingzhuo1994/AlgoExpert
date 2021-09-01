5218225d1e25f82dfdea59133f32c19d2a0568d9
yingzhuo <yingzhuo1994@gmail.com>
2021-08-31 20:09:24 -0500

    Add the 3rd solution

--- WaterArea.py
+++ WaterArea.py
@@ -38,3 +38,27 @@ def waterArea(heights):
             maxes[i] = 0
         rightMax = max(rightMax, height)
     return sum(maxes)
+
+# 3rd solution
+# O(n) time | O(1) space
+def waterArea(heights):
+    if len(heights) == 0:
+        return 0
+    
+    leftIdx = 0
+    rightIdx = len(heights) - 1
+    leftMax = heights[leftIdx]
+    rightMax = heights[rightIdx]
+    surfaceArea = 0
+
+    while leftIdx < rightIdx:
+        if heights[leftIdx] < heights[rightIdx]:
+            leftIdx += 1
+            leftMax = max(leftMax, heights[leftIdx])
+            surfaceArea += leftMax - heights[leftIdx]
+        else:
+            rightIdx -= 1
+            rightMax = max(rightMax, heights[rightIdx])
+            surfaceArea += rightMax - heights[rightIdx]
+
+    return surfaceArea
