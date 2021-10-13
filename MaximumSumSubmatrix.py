466a62be42085953278dedd21bfa9f8b1e798a89
yingzhuo <yingzhuo1994@gmail.com>
2021-10-13 08:45:21 -0500

    official solution

--- MaximumSumSubmatrix.py
+++ MaximumSumSubmatrix.py
@@ -18,6 +18,8 @@ def getSum(matrix, rowOne, rowTwo, colOne, colTwo):
     return result
 
 # 2nd solution, dp
+# O(w * h) time | O(w * h) space
+# where w is the width of the matrix and h is the height
 def maximumSumSubmatrix(matrix, size):
     m = len(matrix)
     n = len(matrix[0])
@@ -40,3 +42,49 @@ def getSum(matrix, rowOne, rowTwo, colOne, colTwo):
         for j in range(colOne, colTwo + 1):
             result += matrix[i][j]
     return result
+
+# 3rd solution, official dp solution
+# O(w * h) time | O(w * h) space
+# where w is the width of the matrix and h is the height
+def maximumSumSubmatrix(matrix, size):
+    sums = createSumMatrix(matrix)
+    maxSubMatrixSum = float("-inf")
+
+    for row in range(size - 1, len(matrix)):
+        for col in range(size - 1, len(matrix[row])):
+            total = sums[row][col]
+
+            touchesTopBorder = row - size < 0
+            if not touchesTopBorder:
+                total -= sums[row - size][col]
+            
+            touchesLeftBorder = col - size < 0
+            if not touchesLeftBorder:
+                total -= sums[row][col - size]
+            
+            touchesTopOrLeftBorder = touchesTopBorder or touchesLeftBorder
+            if not touchesTopOrLeftBorder:
+                total += sums[row - size][col - size]
+            
+            maxSubMatrixSum = max(maxSubMatrixSum, total)
+    
+    return maxSubMatrixSum
+
+def createSumMatrix(matrix):
+    sums = [[0 for _ in range(len(matrix[row]))] for row in range(len(matrix))]
+    sums[0][0] = matrix[0][0]
+
+    # Fill the first Row.
+    for idx in range(1, len(matrix[0])):
+        sums[0][idx] = sums[0][idx - 1] + matrix[0][idx]
+    
+    # Fill the first column.
+    for idx in range(1, len(matrix)):
+        sums[idx][0] = sums[idx - 1][0] + matrix[idx][0]
+    
+    # Fill the rest of the matrix.
+    for row in range(1, len(matrix)):
+        for col in range(1, len(matrix[row])):
+            sums[row][col] = sums[row - 1][col] + sums[row][col - 1] - sums[row - 1][col - 1] + matrix[row][col]
+    
+    return sums
