1b338d6f2eed9fe4df7773ad73b4ad2a5d94a5bd
yingzhuo <yingzhuo1994@gmail.com>
2021-10-14 17:18:17 -0500

    2nd solution

--- MaximizeExpression.py
+++ MaximizeExpression.py
@@ -13,3 +13,34 @@ def maximizeExpression(array):
                     
 def expression(array, a, b, c, d):
     return array[a] - array[b] + array[c] - array[d]
+
+
+# 2nd solution, dynamic programming
+# O(n) time | O(n) space
+def maximizeExpression(array):
+    if len(array) < 4:
+        return 0
+    
+    n = len(array)
+    maxOfA = [array[0]]
+    maxOfAMinusB = [float("-inf")]
+    maxOfAMinusBPlusC = [float("-inf")] * 2
+    maxOfAMinusBPlucCMinusD = [float("-inf")] * 3
+
+    for idx in range(1, n):
+        currentMax = max(maxOfA[idx - 1], array[idx])
+        maxOfA.append(currentMax)
+    
+    for idx in range(1, n):
+        currentMax = max(maxOfAMinusB[idx - 1], maxOfA[idx -1] - array[idx])
+        maxOfAMinusB.append(currentMax)
+
+    for idx in range(2, n):
+        currentMax = max(maxOfAMinusBPlusC[idx - 1], maxOfAMinusB[idx - 1] + array[idx])
+        maxOfAMinusBPlusC.append(currentMax)
+    
+    for idx in range(3, n):
+        currentMax = max(maxOfAMinusBPlucCMinusD[idx - 1], maxOfAMinusBPlusC[idx - 1] - array[idx])
+        maxOfAMinusBPlucCMinusD.append(currentMax)
+
+    return maxOfAMinusBPlucCMinusD[-1]
