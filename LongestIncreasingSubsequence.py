16836c015c3e411201d42ea727c8920c78bebcb0
yingzhuo <yingzhuo1994@gmail.com>
2021-10-17 11:46:51 -0500

    1st solution

--- LongestIncreasingSubsequence.py
+++ LongestIncreasingSubsequence.py
@@ -1,3 +1,19 @@
+# 1st solution
+# O(n^2) time | O(n) space
 def longestIncreasingSubsequence(array):
-    
-
+    dp = [[None, 1] for i in array]
+    largestIdx = 0
+    largestLength = 0
+    for i in range(len(array)):
+        for j in range(i):
+            if array[j] < array[i] and dp[j][1]  + 1 > dp[i][1]:
+                dp[i] = [j, dp[j][1] + 1]
+                largestLength = max(largestLength, dp[i][1])
+        if dp[i][1] >= largestLength:
+            largestIdx = i
+	result = []
+	idx = largestIdx
+    while idx is not None:
+        result.append(array[idx])
+        idx = dp[idx][0]
+    return result[::-1]
