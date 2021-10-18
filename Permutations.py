cdf2cd54f234eb39fa89785218e87e248dd7ca3f
yingzhuo <yingzhuo1994@gmail.com>
2021-10-18 09:19:12 -0500

    3rd solution

--- Permutations.py
+++ Permutations.py
@@ -27,3 +27,22 @@ def permutationsHelper(array, currentPermutation, permutations):
             newArray = array[:i] + array[i + 1:]
             newPermutation = currentPermutation + [array[i]]
             permutationsHelper(newArray, newPermutation, permutations)
+
+# 3rd solution
+# O(n*n!) time | O(n*n!) space
+def getPermutations(array):
+    permutations = []
+    permutationsHelper(0, array, permutations)
+    return permutations
+
+def permutationsHelper(i, array, permutations):
+    if i == len(array) - 1:
+        permutations.append(array[:])
+    else:
+        for j in range(i, len(array)):
+            swap(array, i, j)
+            permutationsHelper(i + 1, array, permutations)
+            swap(array, i, j)
+
+def swap(array, i, j):
+    array[i], array[j] = array[j], array[i]
