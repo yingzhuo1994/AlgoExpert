dcbfd0cb4e652c4b45e3882311b1dd78ddaee685
yingzhuo <yingzhuo1994@gmail.com>
2021-11-03 13:13:33 -0500

    Part solution

--- RectangleMania.py
+++ RectangleMania.py
@@ -1,1 +1,15 @@
 def rectangleMania(coords):
+    coordsTable = getCoordsTable(coords)
+    return getRectangleCount(coords, coordsTable)
+
+def getCoordsTable(coords):
+    coordsTable = {}
+    for coord1 in coords:
+        coord1Directions = {UP: [], RIGHT: [], DOWN: [], LEFT: []}
+        for coord2 in coords:
+            coord2Direction = getCoordDirection(coord1, coord2)
+            if coord2Direction in coord1Directions:
+                coord1Directions[coord2Direction].append(coord2)
+        coord1String = coordToString(coord1)
+        coordsTable[coord1String] = coord1Direction
+    return coordsTable
