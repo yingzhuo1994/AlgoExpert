c90e433a539346c5c51b9e11b24d8b8a05e4af87
yingzhuo <yingzhuo1994@gmail.com>
2021-11-29 09:32:09 -0600

    Two official solutions

--- MergeLinkedLists.py
+++ MergeLinkedLists.py
@@ -19,3 +19,46 @@ def mergeLinkedLists(headOne, headTwo):
         p = p.next
     p.next = headOne or headTwo
     return sentinel.next
+
+# 2nd solution
+# O(m + n) time | O(1) space
+def mergeLinkedLists(headOne, headTwo):
+    p1 = headOne
+    p1Prev = None
+    p2 = headTwo
+    while p1 is not None and p2 is not None:
+        if p1.value < p2.value:
+            p1Prev = p1
+            p1 = p1.next
+        else:
+            if p1Prev is not None:
+                p1Prev.next = p2
+            p1Prev = p2
+            p2 = p2.next
+            p1Prev.next = p1
+    
+    if p1 is None:
+        p1Prev.next = p2
+    return headOne if headOne.value < headTwo.value else headTwo
+
+# 3rd solution, recursion
+# O(m + n) time | O(1) space
+def mergeLinkedLists(headOne, headTwo):
+    recursiveMerge(headOne, headTwo, None)
+    return headOne if headOne.value < headTwo.value else headTwo
+
+def recursiveMerge(p1, p2, p1Prev):
+    if p1 is None:
+        p1Prev.next = p2
+        return 
+    if p2 is None:
+        return 
+    
+    if p1.value < p2.value:
+        recursiveMerge(p1.next, p2, p1)
+    else:
+        if p1Prev is not None:
+            p1Prev.next = p2
+        newP2 = p2.next
+        p2.next = p1
+        recursiveMerge(p1, newP2, p2)
