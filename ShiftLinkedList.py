a5aadddaf45dae98d4f9c3e7a6cc53c1edeabfcd
yingzhuo <yingzhuo1994@gmail.com>
2021-11-30 18:59:02 -0600

    2nd solution

--- ShiftLinkedList.py
+++ ShiftLinkedList.py
@@ -40,3 +40,26 @@ def shiftLinkedList(head, k):
         prev.next = None
     
     return p
+
+# 2nd solution, official
+# O(n) time | O(1) space
+def shiftLinkedList(head, k):
+    listLength = 1
+    listTail = head
+    while listTail.next is not None:
+        listTail = listTail.next
+        listLength += 1
+    
+    offset = abs(k) % listLength
+    if offset == 0:
+        return head
+    
+    newTailPosition = listLength - offset if k > 0 else offset
+    newTail = head
+    for i in range(1, newTailPosition):
+        newTail = newTail.next
+    
+    newHead = newTail.next
+    newTail.next = None
+    listTail.next = head
+    return newHead
