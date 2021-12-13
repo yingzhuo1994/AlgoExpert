b99a907769eb661b931cd1b7225ecde35ae7a6e2
yingzhuo <yingzhuo1994@gmail.com>
2021-12-13 11:37:10 -0600

    1st solution

---
+++ HeapSort.py
@@ -0,0 +1,29 @@
+def heapSort(array):
+    # Write your code here.
+    buildHeap(array)
+    for i in reversed(range(1, len(array))):
+        array[i], array[0] = array[0], array[i]
+        heapify(array, 0, i)
+    return array
+
+
+def heapify(array, idx, end):
+    left = 2 * idx + 1
+    right = 2 * idx + 2
+    largest = idx
+    if left < end and array[largest] < array[left]:
+        largest = left
+
+    if right < end and array[largest] < array[right]:
+        largest = right
+
+    if largest != idx:
+        array[largest], array[idx] = array[idx], array[largest]
+        heapify(array, largest, end)
+
+def buildHeap(array):
+    for i in reversed(range(len(array) // 2)):
+        heapify(array, i, len(array))  
+
+
+    
