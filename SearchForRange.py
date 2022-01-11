4f2278e963e59dd8194dd2598356f3af136e50d4
yingzhuo <yingzhuo1994@gmail.com>
2022-01-11 00:05:10 -0600

    1st solution

--- SearchForRange.py
+++ SearchForRange.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(n) time | O(h) space
+# O(log(n)) time | O(h) space
 def searchForRange(array, target):
     left = binaryLeft(array, target)
     right = binaryRight(array, target)
@@ -24,3 +24,32 @@ def binaryRight(array, target):
         else:
             right = mid - 1
     return left
+
+# 2nd solution
+# O(log(n)) time | O(1) space
+def searchForRange(array, target):
+    finalRange = [-1, -1]
+    alteredBinarySearch(array, target, 0, len(array) - 1, finalRange, True)
+    alteredBinarySearch(array, target, 0, len(array) - 1, finalRange, False)
+    return finalRange
+
+def alteredBinarySearch(array, target, left, right, finalRange, goLeft):
+    while left <= right:
+        mid = (left + right) // 2
+        if array[mid] < target:
+            left = mid + 1
+        elif array[mid] > target:
+            right = mid - 1
+        else:
+            if goLeft:
+                if mid == 0 or array[mid - 1] != target:
+                    finalRange[0] = mid
+                    return 
+                else:
+                    right = mid - 1
+            else:
+                if mid == len(array) - 1 or array[mid + 1] != target:
+                    finalRange[1] = mid
+                    return 
+                else:
+                    left = mid + 1
