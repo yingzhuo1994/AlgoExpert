1e8c40bb7520561b1c11a3ff237b03abf86462f3
yingzhuo <yingzhuo1994@gmail.com>
2022-01-11 23:28:50 -0600

    official solution

--- IndexEqualsValue.py
+++ IndexEqualsValue.py
@@ -13,3 +13,24 @@ def indexEqualsValue(array):
             ans = min(ans, mid)
             right = mid - 1
     return ans if ans != float('inf') else -1
+
+# 2nd solution
+# O(log(n)) time | O(1) space
+def indexEqualsValue(array):
+    leftIndex = 0
+    rightIndex = len(array) - 1
+
+    while leftIndex <= rightIndex:
+        middleIndex = leftIndex + (rightIndex - leftIndex) // 2
+        middleValue = array[middleIndex]
+
+        if middleValue < middleIndex:
+            leftIndex = middleIndex + 1
+        elif middleValue == middleIndex and middleIndex == 0:
+            return middleIndex
+        elif middleValue == middleIndex and array[middleIndex - 1] < middleIndex - 1:
+            return middleIndex
+        else:
+            rightIndex = middleIndex - 1
+    
+    return -1
