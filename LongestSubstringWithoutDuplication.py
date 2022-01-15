0cc59f2e579012fc0ef08d9ef860dd1c66f15740
yingzhuo <yingzhuo1994@gmail.com>
2022-01-15 17:53:12 -0600

    2nd solution

--- LongestSubstringWithoutDuplication.py
+++ LongestSubstringWithoutDuplication.py
@@ -11,3 +11,17 @@ def longestSubstringWithoutDuplication(string):
         if len(string[start:i+1]) > len(ans):
             ans = string[start:i+1]
     return ans
+
+# 2nd official solution
+# O(n) time | O(min(n, a)) space
+def longestSubstringWithoutDuplication(string):
+    lastSeen = {}
+    longest = [0, 1]
+    startIdx = 0
+    for i, char in enumerate(string):
+        if char in lastSeen:
+            startIdx = max(startIdx, lastSeen[char] + 1)
+        if longest[1] - longest[0] < i + 1 - startIdx:
+            longest = [startIdx, i + 1]
+        lastSeen[char] = i
+    return string[longest[0]: longest[1]]
