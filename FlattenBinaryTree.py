bc418f2d572246a2f5747a53ac87aac20767764d
yingzhuo <yingzhuo1994@gmail.com>
2022-01-22 18:03:14 -0600

    add two methods

--- FlattenBinaryTree.py
+++ FlattenBinaryTree.py
@@ -23,3 +23,48 @@ def flattenBinaryTree(root):
         ans[i].right = ans[i + 1]
         ans[i + 1].left = ans[i]
     return ans[0]
+
+# 2nd solution
+# O(n) time | O(n) space
+def flattenBinaryTree(root):
+    inOrderNodes = getNodesInOrder(root, [])
+    for i in range(0, len(inOrderNodes) - 1):
+        leftNode = inOrderNodes[i]
+        rightNode = inOrderNodes[i + 1]
+        leftNode.right = rightNode
+        rightNode.left = leftNode
+    return inOrderNodes[0]
+
+def getNodesInOrder(tree, array):
+    if tree is not None:
+        getNodesInOrder(tree.left, array)
+        array.append(tree)
+        getNodesInOrder(tree.right, array)
+    return array
+
+# 3rd solution
+# O(n) time | O(d) space
+def flattenBinaryTree(root):
+    leftMost, _ = flattenTree(root)
+    return leftMost
+
+def flattenTree(node):
+    if node.left is None:
+        leftMost = node
+    else:
+        leftSubtreeLeftMost, leftSubtreeRightMost = flattenTree(node.left)
+        connectNodes(leftSubtreeRightMost, node)
+        leftMost = leftSubtreeLeftMost
+    
+    if node.right is None:
+        rightMost = node
+    else:
+        rightSubtreeLeftMost, rightSubtreeRightMost = flattenTree(node.right)
+        connectNodes(node, rightSubtreeLeftMost)
+        rightMost = rightSubtreeRightMost
+    
+    return [leftMost, rightMost]
+
+def connectNodes(left, right):
+    left.right = right
+    right.left = left
