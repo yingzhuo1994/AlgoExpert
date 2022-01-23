68d9c8886e521d2900c57c8cc62be9f9cf306f8f
yingzhuo <yingzhuo1994@gmail.com>
2022-01-23 11:45:06 -0600

    4th solution

--- AllKindsOfNodeDepths.py
+++ AllKindsOfNodeDepths.py
@@ -51,3 +51,36 @@ def nodeDepths(node, depth=0):
     if node is None:
         return 0
     return depth + nodeDepths(node.left, depth + 1) + nodeDepths(node.right, depth + 1)
+
+# 4th solution
+# Average case: when the tree is balanced
+# O(n) time | O(n) space
+def allKindsOfNodeDepths(root):
+    nodeCounts = {}
+    addNodeCounts(root, nodeCounts)
+    nodeDepths = {}
+    addNodeDepths(root, nodeDepths, nodeCounts)
+    return sumAllNodeDepths(root, nodeDepths)
+
+def sumAllNodeDepths(node, nodeDepths):
+    if node is None:
+        return 0
+    return sumAllNodeDepths(node.left, nodeDepths) + sumAllNodeDepths(node.right, nodeDepths) + nodeDepths[node]
+
+def addNodeDepths(node, nodeDepths, nodeCounts):
+    nodeDepths[node] = 0
+    if node.left is not None:
+        addNodeDepths(node.left, nodeDepths, nodeCounts)
+        nodeDepths[node] += nodeDepths[node.left] + nodeCounts[node.left]
+    if node.right is not None:
+        addNodeDepths(node.right, nodeDepths, nodeCounts)
+        nodeDepths[node] += nodeDepths[node.right] + nodeCounts[node.right]
+
+def addNodeCounts(node, nodeCounts):
+    nodeCounts[node] = 1
+    if node.left is not None:
+        addNodeCounts(node.left, nodeCounts)
+        nodeCounts[node] += nodeCounts[node.left]
+    if node.right is not None:
+        addNodeCounts(node.right, nodeCounts)
+        nodeCounts[node] += nodeCounts[node.right]
