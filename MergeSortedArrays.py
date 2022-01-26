a4eb80ac40c5480be9271836d65b133a03514a54
yingzhuo <yingzhuo1994@gmail.com>
2022-01-26 11:51:52 -0600

    2nd solution

--- MergeSortedArrays.py
+++ MergeSortedArrays.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(n) time | O(n) space
+# O(nlog(k)) time | O(n) space
 def mergeSortedArrays(arrays):
     n = len(arrays)
     interval = 1
@@ -27,3 +27,82 @@ def mergeTwoArrays(arrays, a, b):
             j += 1
     ans.extend(one[i:] or two[j:])
     return ans
+
+# 2nd solution
+# O(nlog(k) + k) time | O(n + k) space
+# where n is the total number of array elements and k is the number of arrays
+def mergeSortedArrays(arrays):
+    sortedList = []
+    smallestItems = []
+    for arrayIdx in range(len(arrays)):
+        smallestItems.append({"arrayIdx": arrayIdx, "elementIdx": 0, "num": arrays[arrayIdx][0]})
+    minHeap = MinHeap(smallestItems)
+    while not minHeap.isEmpty():
+        smallestItem = minHeap.remove()
+        arrayIdx, elementIdx, num = smallestItem["arrayIdx"], smallestItem["elementIdx"], smallestItem["num"]
+        sortedList.append(num)
+        if elementIdx == len(arrays[arrayIdx]) - 1:
+            continue
+        minHeap.insert({"arrayIdx": arrayIdx, "elementIdx": elementIdx + 1, "num": arrays[arrayIdx][elementIdx + 1]})
+    return sortedList
+
+class MinHeap:
+    def __init__(self, array):
+        # Do not edit the line below.
+        self.heap = self.buildHeap(array)
+
+    def isEmpty(self):
+        return len(self.heap) == 0
+
+    # O(n) time | O(1) space
+    def buildHeap(self, array):
+        # Write your code here.
+        firstParentIdx = (len(array) - 2) // 2
+        for currentIdx in reversed(range(firstParentIdx + 1)):
+            self.siftDown(currentIdx, len(array) - 1, array)
+        return array
+
+    # O(log(n)) time | O(1) space    
+    def siftDown(self, currentIdx, endIdx, heap):
+        # Write your code here.
+        childOneIdx = currentIdx * 2 + 1
+        while childOneIdx <= endIdx:
+            childTwoIdx = currentIdx * 2 + 2 if currentIdx * 2 + 2 <= endIdx else -1
+            if childTwoIdx != -1 and heap[childTwoIdx]["num"] < heap[childOneIdx]["num"]:
+                idxToSwap = childTwoIdx
+            else:
+                idxToSwap = childOneIdx
+            if heap[idxToSwap]["num"] < heap[currentIdx]["num"]:
+                self.swap(currentIdx, idxToSwap, heap)
+                currentIdx = idxToSwap
+                childOneIdx = currentIdx * 2 + 1
+            else:
+                break
+
+    # O(log(n)) time | O(1) space
+    def siftUp(self, currentIdx, heap):
+        # Write your code here.
+        parentIdx = (currentIdx - 1) // 2
+        while currentIdx > 0 and heap[currentIdx]["num"] < heap[parentIdx]["num"]:
+            self.swap(currentIdx, parentIdx, heap)
+            currentIdx = parentIdx
+            parentIdx = (currentIdx - 1) // 2
+
+    def peek(self):
+        # Write your code here.
+        return self.heap[0]
+
+    def remove(self):
+        # Write your code here.
+        self.swap(0, len(self.heap) - 1, self.heap)
+        valueToRemove = self.heap.pop()
+        self.siftDown(0, len(self.heap) - 1, self.heap)
+        return valueToRemove
+
+    def insert(self, value):
+        # Write your code here.
+        self.heap.append(value)
+        self.siftUp(len(self.heap) - 1, self.heap)
+
+    def swap(self, i, j, heap):
+        heap[i], heap[j] = heap[j], heap[i]
