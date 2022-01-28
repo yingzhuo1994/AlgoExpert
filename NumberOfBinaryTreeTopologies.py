7fdde7a6d27a52ce5f6b9ae334b8fc1ae142ad33
yingzhuo <yingzhuo1994@gmail.com>
2022-01-28 15:24:23 -0600

    3 solutions

--- NumberOfBinaryTreeTopologies.py
+++ NumberOfBinaryTreeTopologies.py
@@ -1,3 +1,5 @@
+# 1st solution
+# O(n^2) time | O(n) space
 def numberOfBinaryTreeTopologies(n):
     dic = {0: 1, 1: 1}
     def createBinaryTree(k, dic):
@@ -8,3 +10,44 @@ def numberOfBinaryTreeTopologies(n):
             dic[k] = count
         return dic[k]
     return createBinaryTree(n, dic)
+
+# 2nd solution
+# Upper Bound: O((n*(2n)!)/(n!(n+1)!)) time | O(n) space
+def numberOfBinaryTreeTopologies(n):
+    if n == 0:
+        return 1
+    numberOfTrees = 0
+    for leftTreeSize in range(n):
+        rightTreeSize = n - 1 - leftTreeSize
+        numberOfLeftTrees = numberOfBinaryTreeTopologies(leftTreeSize)
+        numberOfRightTrees = numberOfBinaryTreeTopologies(rightTreeSize)
+        numberOfTrees += numberOfLeftTrees * numberOfRightTrees
+    return numberOfTrees
+
+# 3rd solution
+# O(n^2) time | O(n) space
+def numberOfBinaryTreeTopologies(n, cache={0:1}):
+    if n in cache:
+        return cache[n]
+    numberOfTrees = 0
+    for leftTreeSize in range(n):
+        rightTreeSize = n - 1 - leftTreeSize
+        numberOfLeftTrees = numberOfBinaryTreeTopologies(leftTreeSize, cache)
+        numberOfRightTrees = numberOfBinaryTreeTopologies(rightTreeSize, cache)
+        numberOfTrees += numberOfLeftTrees * numberOfRightTrees
+    cache[n] = numberOfTrees
+    return numberOfTrees
+
+# 4th solution
+# O(n^2) time | O(n) space
+def numberOfBinaryTreeTopologies(n):
+    cache = [1]
+    for m in range(1, n + 1):
+        numberOfTrees = 0
+        for leftTreeSize in range(m):
+            rightTreeSize = m - 1 - leftTreeSize
+            numberOfLeftTrees = cache[leftTreeSize]
+            numberOfRightTrees = cache[rightTreeSize]
+            numberOfTrees += numberOfLeftTrees * numberOfRightTrees
+        cache.append(numberOfTrees)
+    return cache[n]
