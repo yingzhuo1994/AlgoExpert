921f92ddce0428c2b78c2d578541511a2d29aa83
yingzhuo <yingzhuo1994@gmail.com>
2022-01-28 16:20:34 -0600

    3rd solution

--- Non-AttackingQueens.py
+++ Non-AttackingQueens.py
@@ -66,3 +66,46 @@ def isNonAttackingPlacement(row, col, columnPlacements):
         if sameColumn or onDiagonal:
             return False
     return True
+
+# 3rd solution
+# Upper Bound: O(n!) time | O(n) space - where n is the input number
+def nonAttackingQueens(n):
+    blockedColumns = set()
+    blockedUpDiagonals = set()
+    blockedDownDiagonals = set()
+    return getNumberOfNonAttackingQueenPlacements(0, blockedColumns, blockedUpDiagonals, blockedDownDiagonals, n)
+
+def getNumberOfNonAttackingQueenPlacements(row, blockedColumns, blockedUpDiagonals, blockedDownDiagonals, boardSize):
+    if row == boardSize:
+        return 1
+    
+    validPlacements = 0
+    for col in range(boardSize):
+        if isNonAttackingPlacement(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals):
+            placeQueen(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals)
+            validPlacements += getNumberOfNonAttackingQueenPlacements(
+                row + 1, blockedColumns, blockedUpDiagonals, blockedDownDiagonals, boardSize
+            )
+            removeQueen(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals)
+    
+    return validPlacements
+
+# This is always an O(1)-time operation.
+def isNonAttackingPlacement(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals):
+    if col in blockedColumns:
+        return False
+    if row + col in blockedUpDiagonals:
+        return False
+    if row - col in blockedDownDiagonals:
+        return False
+    return True
+
+def placeQueen(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals):
+    blockedColumns.add(col)
+    blockedUpDiagonals.add(row + col)
+    blockedDownDiagonals.add(row - col)
+
+def removeQueen(row, col, blockedColumns, blockedUpDidagonals, blockedDownDiagonals):
+    blockedColumns.remove(col)
+    blockedUpDidagonals.remove(row + col)
+    blockedDownDiagonals.remove(row - col)
