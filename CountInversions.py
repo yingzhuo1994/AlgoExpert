13c45c6de40cf5c67667f13d094163f8325501ee
yingzhuo <yingzhuo1994@gmail.com>
2022-01-28 17:45:31 -0600

    2nd solution

--- CountInversions.py
+++ CountInversions.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(n*log(n)) time | O(n*log(n)) space
+# O(n*log(n)) time | O(n) space
 def countInversions(array):
     ans = [0]
     mergeSort(array, ans)
@@ -35,3 +35,39 @@ def binarySearch(array, num):
         else:
             right = mid
     return left
+
+# 2nd solution
+# O(n*log(n)) time | O(n) space
+def countInversions(array):
+    return countSubArrayInversions(array, 0, len(array))
+
+def countSubArrayInversions(array, start, end):
+    if end - start <= 1:
+        return 0
+    
+    middle = start + (end - start) // 2
+    leftInversions = countSubArrayInversions(array, start, middle)
+    rightInversions = countSubArrayInversions(array, middle, end)
+    mergedArrayInversions = mergeSortAndCountInversions(array, start, middle, end)
+    return leftInversions + rightInversions + mergedArrayInversions
+
+def mergeSortAndCountInversions(array, start, middle, end):
+    sortedArray = []
+    left = start
+    right = middle
+    inversions = 0
+
+    while left < middle and right < end:
+        if array[left] <= array[right]:
+            sortedArray.append(array[left])
+            left += 1
+        else:
+            inversions += middle - left
+            sortedArray.append(array[right])
+            right += 1
+    
+    sortedArray += array[left:middle] + array[right:end]
+    for idx, num in enumerate(sortedArray):
+        array[start + idx] = num
+    
+    return inversions
