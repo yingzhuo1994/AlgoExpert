d9c1f0c80840b450f81eb5825e5ac22287717a8f
yingzhuo <yingzhuo1994@gmail.com>
2022-01-29 16:51:19 -0600

    3rd solution

--- LongestBalancedSubstring.py
+++ LongestBalancedSubstring.py
@@ -43,3 +43,42 @@ def longestBalancedSubstring(string):
                 currentLength = i - balancedSubstringStartIdx
                 maxLength = max(maxLength, currentLength)
     return maxLength
+
+# 3rd solution
+# O(n) time | O(1) space - where n is the length of the input string
+def longestBalancedSubstring(string):
+    maxLength = 0
+
+    openingCount = 0
+    closingCount = 0
+
+    for char in string:
+        if char == "(":
+            openingCount += 1
+        else:
+            closingCount += 1
+        
+        if openingCount == closingCount:
+            maxLength = max(maxLength, closingCount * 2)
+        elif closingCount > openingCount:
+            openingCount = 0
+            closingCount = 0
+    
+    openingCount = 0
+    closingCount = 0
+
+    for i in reversed(range(len(string))):
+        char = string[i]
+
+        if char == "(":
+            openingCount += 1
+        else:
+            closingCount += 1
+        
+        if openingCount == closingCount:
+            maxLength = max(maxLength, closingCount * 2)
+        elif openingCount > closingCount:
+            openingCount = 0
+            closingCount = 0
+    
+    return maxLength
