b686312242b73920d3e4c9156b85654cfcf1449b
yingzhuo <yingzhuo1994@gmail.com>
2022-01-29 17:02:34 -0600

    4th solution

--- LongestBalancedSubstring.py
+++ LongestBalancedSubstring.py
@@ -82,3 +82,40 @@ def longestBalancedSubstring(string):
             closingCount = 0
     
     return maxLength
+
+# 4th solution
+# O(n) time | O(1) space - where n is the length of the input string
+def longestBalancedSubstring(string):
+    return max(
+        getLongestBalancedInDirection(string, True),
+        getLongestBalancedInDirection(string, False)
+    )
+
+def getLongestBalancedInDirection(string, leftToRight):
+    openingParens = "(" if leftToRight else ")"
+    startIdx = 0 if leftToRight else len(string) - 1
+    step = 1 if leftToRight else -1
+
+    maxLength = 0
+
+    openingCount = 0
+    closingCount = 0
+
+    idx = startIdx
+    while idx >= 0 and idx < len(string):
+        char = string[idx]
+
+        if char == openingParens:
+            openingCount += 1
+        else:
+            closingCount += 1
+        
+        if openingCount == closingCount:
+            maxLength = max(maxLength, closingCount * 2)
+        elif closingCount > openingCount:
+            openingCount = 0
+            closingCount = 0
+
+        idx += step
+    
+    return maxLength
