ecf5c986ef144b442f6984761cf7da39c39de1e3
yingzhuo <yingzhuo1994@gmail.com>
2022-01-30 20:16:12 -0600

    two solutions

---
+++ ValidateSubsequence.py
@@ -0,0 +1,23 @@
+# 1st solution
+# O(n) time | O(1) space
+# where n is the length of the array
+def isValidSubsequence(array, sequence):
+    arrIdx = 0
+    seqIdx = 0
+    while arrIdx < len(array) and seqIdx < len(sequence):
+        if array[arrIdx] == sequence[seqIdx]:
+            seqIdx += 1
+        arrIdx += 1
+    return seqIdx == len(sequence)
+
+# 2nd solution
+# O(n) time | O(1) space
+# where n is the length of the array
+def isValidSubsequence(array, sequence):
+    seqIdx = 0
+    for value in array:
+        if seqIdx == len(sequence):
+            break
+        if sequence[seqIdx] == value:
+            seqIdx += 1
+    return seqIdx == len(sequence)
