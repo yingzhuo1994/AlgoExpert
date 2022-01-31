bb2e8e2b0fe458f018014c9860cf452b099ee182
yingzhuo <yingzhuo1994@gmail.com>
2022-01-31 14:01:58 -0600

    1st solution

---
+++ SortedSquaredArray.py
@@ -0,0 +1,19 @@
+# O(n) time | O(1) space
+# where n is the length of the input array
+def sortedSquaredArray(array):
+    sortedSquares = [0 for _ in array]
+    smallerValueIdx = 0
+    largerValueIdx = len(array) - 1
+
+    for idx in reversed(range(len(array))):
+        smallerValue = array[smallerValueIdx]
+        largerValue = array[largerValueIdx]
+
+        if abs(smallerValue) > abs(largerValue):
+            sortedSquares[idx] = smallerValue * smallerValue
+            smallerValueIdx += 1
+        else:
+            sortedSquares[idx] = largerValue * largerValue
+            largerValueIdx -= 1
+    
+    return sortedSquares
