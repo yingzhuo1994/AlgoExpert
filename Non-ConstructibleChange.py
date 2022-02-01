70c9f81070d0115d2d6e568b0b025af410526228
yingzhuo <yingzhuo1994@gmail.com>
2022-02-01 11:31:36 -0600

    complexity analysis

--- Non-ConstructibleChange.py
+++ Non-ConstructibleChange.py
@@ -1,21 +1,5 @@
-# def nonConstructibleChange(coins):
-#     # Write your code here.
-#     coins.sort()
-#     amount = 1
-#     allvalue = [sum(lst) for lst in allChange(coins)]
-#     while True:
-#         if amount in allvalue:
-#             amount += 1
-#         else:
-#             return amount
-#
-# def allChange(coins):
-#     if not coins:
-#         return [[]]
-#     withcoin = [[coins[0]] + elem for elem in allChange(coins[1:])]
-#     withoutcoin= allChange(coins[1:])
-#     return withcoin + withoutcoin
-
+# O(n) time | O(1) space
+# where n is the number of coins
 def nonConstructibleChange(coins):
     coins.sort()
     amount = 0
@@ -24,10 +8,3 @@ def nonConstructibleChange(coins):
             return amount + 1
         amount += coin
     return amount + 1
-
-
-coins = [5, 7, 1, 1, 2, 3, 22]
-# for lst in allChange(coins):
-    # print(lst)
-# print([sum(lst) for lst in allChange(coins)])
-print(nonConstructibleChange(coins))
