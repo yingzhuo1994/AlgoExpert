b0ec8317932445c6fb1288cc03b9c3d4075b6029
yingzhuo <yingzhuo1994@gmail.com>
2022-02-01 12:09:35 -0600

    two official solutions

--- SpiralTraverse.py
+++ SpiralTraverse.py
@@ -31,6 +31,7 @@ def recursiveSpiral(array, lst, row, column):
         recursiveSpiral(array, lst, 0, 0)
 
 ### Joey Iteration solution
+# O(n) time | O(n) space
 def spiralTraverse(array):
     # Write your code here.
     n = len(array)
@@ -63,3 +64,83 @@ def spiralTraverse(array):
             startColumn += 1
             i = 0
     return lst
+
+# O(n) time | O(n) space
+# where n is the total number of elements in the array
+def spiralTraverse(array):
+    result = []
+    startRow, endRow = 0, len(array) - 1
+    startCol, endCol = 0, len(array[0]) - 1
+
+    while startRow <= endRow and startCol <= endCol:
+        for col in range(startCol, endCol + 1):
+            result.append(array[startRow][col])
+        
+        for row in range(startRow + 1, endRow + 1):
+            result.append(array[row][endCol])
+        
+        for col in reversed(range(startCol, endCol)):
+            # Handle the edge case when there's a single row
+            # in the middle of the matrix. In this case, we don't
+            # want to double-count the values in this row, which
+            # we've already counted in the first for loop above.
+            # See Test Case 8 for an example of this edge case.
+            if startRow == endRow:
+                break
+            result.append(array[endRow][col])
+        
+        for row in reversed(range(startRow + 1, endRow)):
+            # Handle the edge case when there's a single column
+            # in the middle of the matrix. In this case, we don't
+            # want to double-count the values in this column, which
+            # we've readly counted in the second for loop above.
+            # See Test Case 9 for an example of this edge case.
+            if startCol == endCol:
+                break
+            result.append(array[row][startCol])
+    
+        startRow += 1
+        endRow -= 1
+        startCol += 1
+        endCol -= 1
+
+    return result
+
+# O(n) time | O(n) space
+# where n is the total number of elements in the array
+def spiralTraverse(array):
+    result = []
+    spiralFill(array, 0, len(array) - 1, 0, len(array[0]) - 1, result)
+    return result
+
+def spiralFill(array, startRow, endRow, startCol, endCol, result):
+    if startRow > endRow or startCol > endCol:
+        return 
+    
+    for col in range(startCol, endCol + 1):
+        result.append(array[startRow][col])
+    
+    for row in range(startRow + 1, endRow + 1):
+        result.append(array[row][endCol])
+    
+    for col in reversed(range(startCol, endCol)):
+        # Handle the edge case when there's a single row
+        # in the middle of the matrix. In this case, we don't
+        # want to double-count the values in this row, which
+        # we've already counted in the first for loop above.
+        # See Test Case 8 for an example of this edge case.
+        if startRow == endRow:
+            break
+        result.append(array[endRow][col])
+    
+    for row in reversed(range(startRow + 1, endRow)):
+        # Handle the edge case when there's a single column
+        # in the middle of the matrix. In this case, we don't
+        # want to double-count the values in this row, which
+        # we've already counted in the second for loop above.
+        # See Test Case 9 for an example of this edge case.
+        if startCol == endCol:
+            break
+        result.append(array[row][startCol])
+    
+    spiralFill(array, startRow + 1, endRow - 1, startCol + 1, endCol - 1, result)
