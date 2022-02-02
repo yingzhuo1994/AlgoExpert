b0a65250ff0e8a6d0d70cc0c91498b6e7353166b
yingzhuo <yingzhuo1994@gmail.com>
2022-02-02 11:19:53 -0600

    2 solutions

--- FindClosestValueInBst.py
+++ FindClosestValueInBst.py
@@ -1,19 +1,39 @@
-# def findClosestValueInBst(tree, target):
-#     # Write your code here.
-#     dic = {}
-#     def helper(tree, target):
-#         nonlocal dic
-#         if not tree:
-#             return
-#         dic[tree.value] = abs(target - tree.value)
-#         if target == tree.value:
-#             return
-#         elif target < tree.value:
-#             helper(tree.left, target)
-#         else:
-#             helper(tree.right, target)
-#     helper(tree, target)
-#     return min(dic, key = lambda k: dic[k])
+# 1st solution
+# Average: O(log(n)) time | O(log(n)) space
+# Worst: O(n) time | O(n) space
+def findClosestValueInBst(tree, target):
+    return findClosestValueInBstHelper(tree, target, tree.value)
+
+def findClosestValueInBstHelper(tree, target, closest):
+    if tree is None:
+        return closest
+    if abs(target - closest) > abs(target - tree.value):
+        closest = tree.value
+    if target < tree.value:
+        return findClosestValueInBstHelper(tree.left, target, closest)
+    elif target > tree.value:
+        return findClosestValueInBstHelper(tree.right, target, closest)
+    else:
+        return closest
+
+# 2nd solution
+# Average: O(log(n)) time | O(log(n)) space
+# Worst: O(n) time | O(n) space
+def findClosestValueInBst(tree, target):
+    return findClosestValueInBstHelper(tree, target, tree.value)
+
+def findClosestValueInBstHelper(tree, target, closest):
+    currentNode = tree
+    while currentNode is not None:
+        if abs(target - closest) > abs(target - currentNode.value):
+            closest = currentNode.value
+        if target < currentNode.value:
+            currentNode = currentNode.left
+        elif target > currentNode.value:
+            currentNode = currentNode.right
+        else:
+            break
+    return closest
 
 def findClosestValueInBst(tree, target):
     # Write your code here.
