e406ce92ac156cfeb76fedb61464ace57d2d1c16
yingzhuo <yingzhuo1994@gmail.com>
2022-02-02 17:06:22 -0600

    2nd solution

--- BoggleBoard.py
+++ BoggleBoard.py
@@ -56,3 +56,54 @@ class TrieNode:
     def __init__(self):
         self.dic = {}
         self.end = False
+
+# 2nd solution
+# O(nm*8^s + ws) time | O(nm + ws) space
+def boggleBoard(board, words):
+	trie = Trie()
+	for word in words:
+		trie.add(word)
+	finalWords = {}
+	visited = [[False for letter in row] for row in board]
+	for i in range(len(board)):
+		for j in range(len(board[i])):
+			explore(i, j, board, trie.root, visited, finalWords)
+	return list(finalWords.keys())
+
+def explore(i, j, board, trieNode, visited, finalWords):
+	if visited[i][j]:
+		return
+	letter = board[i][j]
+	if letter not in trieNode:
+		return 
+	visited[i][j] = True
+	trieNode = trieNode[letter]
+	if "*" in trieNode:
+		finalWords[trieNode["*"]] = True
+	neighbors = getNeighbors(i, j, board)
+	for neighbor in neighbors:
+		explore(neighbor[0], neighbor[1], board, trieNode, visited, finalWords)
+	visited[i][j] = False
+
+def getNeighbors(i, j, board):
+	neighbors = []
+	for x in (-1, 0, 1):
+		for y in (-1, 0, 1):
+			if (x, y) != (0, 0):
+				row, col = i + x, j + y
+				if 0 <= row and row < len(board) and 0 <= col and col < len(board[0]):
+					neighbors.append([row, col])
+	return neighbors
+
+class Trie:
+	def __init__(self):
+		self.root = {}
+		self.endSymbol = "*"
+	
+	def add(self, word):
+		current = self.root
+		for letter in word:
+			if letter not in current:
+				current[letter] = {}
+			current = current[letter]
+		current[self.endSymbol] = word
