30b4d71f157793dc63ac731a4b4e1953e606e57c
yingzhuo <yingzhuo1994@gmail.com>
2022-02-02 15:36:10 -0600

    renew

--- ArrayOfProducts.py
+++ ArrayOfProducts.py
@@ -1,32 +1,56 @@
+# 1st solution
+# O(n^2) time | O(n) space
+# where n is the length of the input array
 def arrayOfProducts(array):
-    # Write your code here.
-    # lst = []
-    # for i in range(len(array)):
-    #     products = 1
-    #     for j in range(len(array)):
-    #         if j != i:
-    #             products *= array[j]
-    #     lst.append(products)
-    # return lst
+    products = [1 for _ in array]
 
-    # 2nd solution
-    # lst = []
-    # left, right = 1, 1
-    # for i in range(len(array)):
-    #     lst.append(left)
-    #     left *= array[i]
-    # for i in reversed(range(len(array))):
-    #     lst[i] *= right
-    #     right *= array[i]
-    # return lst
-
-    # 3rd solution
-    n = len(array)
-    lst = [1 for _ in range(n)]
-    left, right = 1, 1
     for i in range(len(array)):
-        lst[i] *= left
-        lst[n-1-i] *= right
-        left *= array[i]
-        right *= array[n-1-i]
-    return lst
+        runningProduct = 1
+        for j in range(len(array)):
+            if i != j:
+                runningProduct *= array[j]
+        products[i] = runningProduct
+    
+    return products
+
+# 2nd solution
+# O(n) time | O(n) space
+# where n is the length of the input array
+def arrayOfProducts(array):
+    products = [1 for _ in range(len(array))]
+    leftProducts = [1 for _ in range(len(array))]
+    rightProducts = [1 for _ in range(len(array))]
+
+    leftRunningProduct = 1
+    for i in range(len(array)):
+        leftProducts[i] = leftRunningProduct
+        leftRunningProduct *= array[i]
+    
+    rightRunningProduct = 1
+    for i in reversed(range(len(array))):
+        rightProducts[i] = rightRunningProduct
+        rightRunningProduct *= array[i]
+    
+    for i in range(len(array)):
+        products[i] = leftProducts[i] * rightProducts[i]
+    
+    return products
+
+
+# 3rd solution
+# O(n) time | O(n) space
+# where n is the length of the input array
+def arrayOfProducts(array):
+    products = [1 for _ in range(len(array))]
+
+    leftRunningProduct = 1
+    for i in range(len(array)):
+        products[i] = leftRunningProduct
+        leftRunningProduct *= array[i]
+    
+    rightRunningProduct = 1
+    for i in reversed(range(len(array))):
+        products[i] *= rightRunningProduct
+        rightRunningProduct *= array[i]
+    
+    return products
