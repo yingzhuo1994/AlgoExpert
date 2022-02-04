3a6ad4397e12b393f07a4661e843cc02b18b6459
yingzhuo <yingzhuo1994@gmail.com>
2022-02-04 13:32:17 -0600

    complexity analysis

--- ValidateBST.py
+++ ValidateBST.py
@@ -5,16 +5,18 @@ class BST:
         self.left = None
         self.right = None
 
-
+# O(n) time | O(d) space
 def validateBst(tree):
     # Write your code here.
-    return helper(tree, float('-inf'), float('inf'))
+    return validateBstHelper(tree, float('-inf'), float('inf'))
 
-def helper(tree, minValue, maxValue):
-    if not tree:
+def validateBstHelper(tree, minValue, maxValue):
+    if tree is None:
         return True
     if tree.value < minValue or tree.value >= maxValue:
         return False
-    leftIsValid = helper(tree.left, minValue, tree.value)
-    rightIsValid = helper(tree.right, tree.value, maxValue)
-    return leftIsValid and rightIsValid
+    leftIsValid = validateBstHelper(tree.left, minValue, tree.value)
+    if not leftIsValid:
+        return False
+    rightIsValid = validateBstHelper(tree.right, tree.value, maxValue)
+    return rightIsValid
