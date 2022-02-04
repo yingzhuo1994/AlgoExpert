d6eea5ec807700a0cbb2de59e6089eb5ca479afc
yingzhuo <yingzhuo1994@gmail.com>
2022-02-04 12:10:02 -0600

    2nd solution

--- NodeDepths.py
+++ NodeDepths.py
@@ -1,3 +1,5 @@
+# 1st solution, Recursion
+# O(n) time | O(h) space 
 def nodeDepths(root):
     # Write your code here.
     return singleNodeDepth(root, 0)
@@ -7,6 +9,20 @@ def singleNodeDepth(tree, depth):
 		return 0
 	else:
 		return depth + singleNodeDepth(tree.left, depth + 1) + singleNodeDepth(tree.right, depth + 1)
+
+# 2nd solution, Iteration
+# O(n) time | O(h) space 
+def nodeDepths(root):
+	stack = [(root, 0)]
+	ans = 0
+	while stack:
+		node, depth = stack.pop()
+		ans += depth
+		if node.right:
+			stack.append((node.right, depth + 1))
+		if node.left:
+			stack.append((node.left, depth + 1))
+	return ans
 
 # This is the class of the input binary tree.
 class BinaryTree:
