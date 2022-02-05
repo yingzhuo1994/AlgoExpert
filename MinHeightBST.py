593ef297e462b42cd66dedde718010019deda96a
yingzhuo <yingzhuo1994@gmail.com>
2022-02-05 10:13:12 -0600

    renew

--- MinHeightBST.py
+++ MinHeightBST.py
@@ -1,11 +1,57 @@
+# 1st solution
+# O(nlog(n)) time | O(n) space - where n is the length of the array
 def minHeightBst(array):
-    if not array:
-        return None
-    mid = len(array) // 2
-    head = BST(array[mid])
-    head.left = minHeightBst(array[:mid])
-    head.right = minHeightBst(array[mid+1:])
-    return head
+    return constructMinHeightBst(array, None, 0, len(array) - 1)
+
+def constructMinHeightBst(array, bst, startIdx, endIdx):
+    if endIdx < startIdx:
+        return
+    midIdx = (startIdx + endIdx) // 2
+    valueToAdd = array[midIdx]
+    if bst is None:
+        bst = BST(valueToAdd)
+    else:
+        bst.insert(valueToAdd)
+    constructMinHeightBst(array, bst, startIdx, midIdx - 1)
+    constructMinHeightBst(array, bst, midIdx + 1, endIdx)
+    return bst
+
+# 2nd solution
+# O(n) time | O(n) space - where n is the length of the array
+def minHeightBst(array):
+    return constructMinHeightBst(array, None, 0, len(array) - 1)
+
+def constructMinHeightBst(array, bst, startIdx, endIdx):
+    if endIdx < startIdx:
+        return
+    midIdx = (startIdx + endIdx) // 2
+    newBstNode = BST(array[midIdx])
+    if bst is None:
+        bst = newBstNode
+    else:
+        if array[midIdx] < bst.value:
+            bst.left = newBstNode
+            bst = bst.left
+        else:
+            bst.right = newBstNode
+            bst = bst.right
+    constructMinHeightBst(array, bst, startIdx, midIdx - 1)
+    constructMinHeightBst(array, bst, midIdx + 1, endIdx)
+    return bst
+
+# 3rd solution
+# O(n) time | O(n) space - where n is the length of the array
+def minHeightBst(array):
+    return constructMinHeightBst(array, 0, len(array) - 1)
+
+def constructMinHeightBst(array, startIdx, endIdx):
+    if endIdx < startIdx:
+        return
+    midIdx = (startIdx + endIdx) // 2
+    bst = BST(array[midIdx])
+    bst.left = constructMinHeightBst(array, startIdx, midIdx - 1)
+    bst.right = constructMinHeightBst(array, midIdx + 1, endIdx)
+    return bst
 
 
 class BST:
