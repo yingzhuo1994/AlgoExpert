4437caada6160e85cb02b818a133253071ada241
yingzhuo <yingzhuo1994@gmail.com>
2022-02-05 20:10:30 -0600

    two solutions

--- MultiStringSearch.py
+++ MultiStringSearch.py
@@ -1,5 +1,7 @@
 # 1st solution
 # O(bns) time | O(n) space
+# where n is the length of smallStrings, b is the length of bigString,
+# and s is the longest length of smallStrings.
 def multiStringSearch(bigString, smallStrings):
     return [isInBigString(bigString, smallString) for smallString in smallStrings]
 
@@ -24,3 +26,72 @@ def isInBigStringHelper(bigString, smallString, startIdx):
         leftSmallIdx += 1
         rightSmallIdx -= 1
     return True
+
+# 2nd solution
+# O(b^2 + ns) time | O(b^2 + n) space
+# where n is the length of smallStrings, b is the length of bigString,
+# and s is the longest length of smallStrings.
+def multiStringSearch(bigString, smallStrings):
+    modifiedSuffixTrie = ModifiedSuffixTrie(bigString)
+    return [modifiedSuffixTrie.contains(string) for string in smallStrings]
+
+class ModifiedSuffixTrie:
+    def __init__(self, string):
+        self.root = {}
+        self.populateModifiedSuffixTrieFrom(string)
+    
+    def populateModifiedSuffixTrieFrom(self, string):
+        for i in range(len(string)):
+            self.insertSubstringStartingAt(i, string)
+    
+    def insertSubstringStartingAt(self, i, string):
+        node = self.root
+        for j in range(i, len(string)):
+            letter = string[j]
+            if letter not in node:
+                node[letter] = {}
+            node = node[letter]
+    
+    def contains(self, string):
+        node = self.root
+        for letter in string:
+            if letter not in node:
+                return False
+            node = node[letter]
+        return True
+
+# 3rd solution
+# O(ns + bs) time | O(ns) space
+# where n is the length of smallStrings, b is the length of bigString,
+# and s is the longest length of smallStrings.
+def multiStringSearch(bigString, smallStrings):
+    trie = Trie()
+    for string in smallStrings:
+        trie.insert(string)
+    containedStrings = {}
+    for i in range(len(bigString)):
+        findSmallStringsIn(bigString, i, trie, containedStrings)
+    return [string in containedStrings for string in smallStrings]
+
+def findSmallStringsIn(string, startIdx, trie, containedStrings):
+    currentNode = trie.root
+    for i in range(startIdx, len(string)):
+        currentChar = string[i]
+        if currentChar not in currentNode:
+            break
+        currentNode = currentNode[currentChar]
+        if trie.endSymbol in currentNode:
+            containedStrings[currentNode[trie.endSymbol]] = True
+
+class Trie:
+    def __init__(self):
+        self.root = {}
+        self.endSymbol = "*"
+    
+    def insert(self, string):
+        current = self.root
+        for i in range(len(string)):
+            if string[i] not in current:
+                current[string[i]] = {}
+            current = current[string[i]]
+        current[self.endSymbol] = string
