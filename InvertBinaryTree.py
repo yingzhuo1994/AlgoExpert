c99e0a93f69e24402d38b6a19e580708a061bb3b
yingzhuo <yingzhuo1994@gmail.com>
2022-02-06 10:52:31 -0600

    add one solution

--- InvertBinaryTree.py
+++ InvertBinaryTree.py
@@ -1,8 +1,24 @@
+# 1st solution
+# O(n) time | O(n) space
 def invertBinaryTree(tree):
-    # Write your code here.
-    # O(n)
+    queue = [tree]
+    while len(queue):
+        current = queue.pop(0)
+        if current is None:
+            continue
+        swapLeftAndRight(current)
+        queue.append(current.left)
+        queue.append(current.right)
+
+def swapLeftAndRight(tree):
+    tree.left, tree.right = tree.right, tree.left
+
+
+# 2nd solution
+# O(n) time | O(d) space
+def invertBinaryTree(tree):
     if not tree:
-        return None
+        return
     tree.left, tree.right = tree.right, tree.left
     invertBinaryTree(tree.left)
     invertBinaryTree(tree.right)
