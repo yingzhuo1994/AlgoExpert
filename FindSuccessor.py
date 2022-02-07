38afe9d4d06c5c9f1b45d3d7c59d33057128776b
yingzhuo <yingzhuo1994@gmail.com>
2022-02-07 09:42:06 -0600

    renew

--- FindSuccessor.py
+++ FindSuccessor.py
@@ -7,25 +7,33 @@ class BinaryTree:
         self.parent = parent
 
 # 1st solution
+# O(n) time | O(n) space
+# where n is the number of nodes in the tree
 def findSuccessor(tree, node):
-    # Write your code here.
-    array = inOrderTraversal(tree)
-    for i in range(len(array) - 1):
-        if array[i] == node:
-            return array[i + 1]
-    return None
+    inOrderTraversalOrder = getInOrderTraversalOrder(tree)
 
-def inOrderTraversal(tree, array = []):
-    if not tree:
-        return []
-    inOrderTraversal(tree.left)
-    array.append(tree)
-    inOrderTraversal(tree.right)
-    return array
+    for idx, currentNode in enumerate(inOrderTraversalOrder):
+        if currentNode != node:
+            continue
+
+        if idx == len(inOrderTraversalOrder) - 1:
+            return None
+        
+        return inOrderTraversalOrder[idx + 1]
+
+def getInOrderTraversalOrder(node, order = []):
+    if node is None:
+        return order
+    getInOrderTraversalOrder(node.left, order)
+    order.append(node)
+    getInOrderTraversalOrder(node.right, order)
+
+    return order
 
 # 2nd solution
+# O(h) time | O(1) space
+# where h is the height of the tree
 def findSuccessor(tree, node):
-    # Write your code here.
     if node.right:
         p = node.right
         while p.left:
@@ -36,3 +44,25 @@ def findSuccessor(tree, node):
         while p.parent and p.parent.right == p:
             p = p.parent
         return p.parent
+
+# 3rd solution
+# O(h) time | O(1) space
+# where h is the height of the tree
+def findSuccessor(tree, node):
+    if node.right is not None:
+        return getLeftmostChild(node.right)
+    
+    return getRightmostParent(node)
+
+def getLeftmostChild(node):
+    currentNode = node
+    while currentNode.left is not None:
+        currentNode = currentNode.left
+    return currentNode
+
+def getRightmostParent(node):
+    currentNode = node
+    while currentNode.parent is not None and currentNode.parent.right == currentNode:
+        currentNode = currentNode.parent
+    
+    return currentNode.parent
