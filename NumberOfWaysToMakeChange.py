3ffee508f7e5d480ea4970dff8874afc9f99a94e
yingzhuo <yingzhuo1994@gmail.com>
2022-02-08 11:17:29 -0600

    renew

--- NumberOfWaysToMakeChange.py
+++ NumberOfWaysToMakeChange.py
@@ -1,13 +1,9 @@
+# O(nd) time | O(n) space
 def numberOfWaysToMakeChange(n, denoms):
-    # Write your code here.
-    if n == 0:
-        return 1
-    elif n < 0:
-        return 0
-    results = [0 for _ in range(n + 1)]
-    results[0] = 1
-    for value in denoms:
-        for i in range(1, n + 1):
-            if value <= i:
-                results[i] += results[i - value]
-    return results[n]
+    ways = [0 for _ in range(n + 1)]
+    ways[0] = 1
+    for denom in denoms:
+        for amount in range(1, n + 1):
+            if denom <= amount:
+                ways[amount] += ways[amount - denom]
+    return ways[n]
