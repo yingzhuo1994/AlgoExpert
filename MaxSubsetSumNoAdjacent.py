61216e57925bdfa0cee81c0939bbfd056140bdba
yingzhuo <yingzhuo1994@gmail.com>
2022-02-08 11:11:00 -0600

    renew

--- MaxSubsetSumNoAdjacent.py
+++ MaxSubsetSumNoAdjacent.py
@@ -1,7 +1,6 @@
+# 1st recursive solution
+# O(2^n) time | O(n) space
 def maxSubsetSumNoAdjacent(array):
-    # Write your code here.
-    # 1st recursive solution
-    # O(2^n) time | O(n) space
     if not array:
         return 0
     if len(array) == 1:
@@ -10,8 +9,22 @@ def maxSubsetSumNoAdjacent(array):
     withoutFirst = maxSubsetSumNoAdjacent(array[1:])
     return max(withFirst, withoutFirst)
 
-    # 2nd iterative solution
-    # O(n) time | O(1) space
+# 2nd iterative solution
+# O(n) time | O(n) space
+def maxSubsetSumNoAdjacent(array):
+    if not array:
+        return 0
+    elif len(array) == 1:
+        return array[0]
+    maxSums = array[:]
+    maxSums[1] = max(array[0], array[1])
+    for i in range(2, len(array)):
+        maxSums[i] = max(maxSums[i - 1], maxSums[i - 2] + array[i])
+    return maxSums[-1]
+
+# 3rd iterative solution
+# O(n) time | O(1) space
+def maxSubsetSumNoAdjacent(array):
     if not array:
         return 0
     elif len(array) == 1:
