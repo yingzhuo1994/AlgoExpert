93e286a637ed8a63b8f885586f550a44d74089b7
yingzhuo <yingzhuo1994@gmail.com>
2022-02-10 10:16:49 -0600

    renew

--- CaesarCipherEncrptor.py
+++ CaesarCipherEncrptor.py
@@ -1,21 +1,24 @@
+# 1st solution
+# O(n) time | O(n) space
 def caesarCipherEncryptor(string, key):
-    # Write your code here.
-    # start = ord('a')
-    # end = ord('z')
-    # lst = []
-    # for ch in string:
-    #     newNum = ord(ch) + key
-    #     while newNum > end:
-    #         newNum = newNum - end + start - 1
-    #     lst.append(chr(newNum))
-    # return ''.join(lst)
-
-    # 2nd Solution
     start = ord('a')
+    end = ord('z')
+    key %= 26
+    lst = []
+    for ch in string:
+        newNum = ord(ch) + key
+        while newNum > end:
+            newNum = newNum - end + start - 1
+        lst.append(chr(newNum))
+    return ''.join(lst)
+
+# 2nd Solution
+# O(n) time | O(n) space
+def caesarCipherEncryptor(string, key):
+    start = ord('a')
+    key %= 26
     lst = []
     for ch in string:
         num = start + (ord(ch) + key - start) % 26
         lst.append(chr(num))
     return ''.join(lst)
-
-print(caesarCipherEncryptor('z', 1))
