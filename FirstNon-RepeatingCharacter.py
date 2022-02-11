68e60403b3fa4f1e542d2df9b27ffa59e9e63bc9
yingzhuo <yingzhuo1994@gmail.com>
2022-02-11 09:52:49 -0600

    2nd solution

--- FirstNon-RepeatingCharacter.py
+++ FirstNon-RepeatingCharacter.py
@@ -1,6 +1,21 @@
+# 1st solution
+# O(n) time | O(1) space
 def firstNonRepeatingCharacter(string):
-    # Write your code here.
-    # O(n) time | O(1) space
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     index=[string.index(l) for l in alphabet if string.count(l) == 1]
     return min(index) if len(index) > 0 else -1
+
+# 2nd solution
+# O(n) time | O(1) space
+def firstNonRepeatingCharacter(string):
+    characterFrequencies = {}
+
+    for character in string:
+        characterFrequencies[character] = characterFrequencies.get(character, 0) + 1
+    
+    for idx in range(len(string)):
+        character = string[idx]
+        if characterFrequencies[character] == 1:
+            return idx
+    
+    return -1
