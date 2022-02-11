9145cc0005cd557b76a3a220afa517aa7115a5aa
yingzhuo <yingzhuo1994@gmail.com>
2022-02-11 10:07:59 -0600

    renew

--- RiverSizes.py
+++ RiverSizes.py
@@ -1,5 +1,6 @@
+# 1st solution
+# O(wh) time | O(wh) space
 def riverSizes(matrix):
-    # Write your code here.
     lengthLst = []
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
@@ -18,4 +19,40 @@ def lookRiver(matrix, x, y):
     else:
         return 0
 
+# 2nd solution
+# O(wh) time | O(wh) space
+def riverSizes(matrix):
+    sizes = []
+    visited = [[False for value in row] for row in matrix]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if visited[i][j]:
+                continue
+            traverseNode(i, j, matrix, visited, sizes)
+    return sizes
 
+def traverseNode(i, j, matrix, visited, sizes):
+    currentRiverSize = 0
+    nodesToExplore = [[i, j]]
+    while len(nodesToExplore):
+        currentNode = nodesToExplore.pop()
+        i = currentNode[0]
+        j = currentNode[1]
+        if visited[i][j]:
+            continue
+        visited[i][j] = True
+        if matrix[i][j] == 0:
+            continue
+        currentRiverSize += 1
+        unvisitedNeighbors = getUnvisitedNeighbors(i, j, matrix, visited)
+        for neighbor in unvisitedNeighbors:
+            nodesToExplore.append(neighbor)
+    if currentRiverSize > 0:
+        sizes.append(currentRiverSize)
+
+def getUnvisitedNeighbors(i, j, matrix, visited):
+    unvisitedNeighbors = []
+    for row, col in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+        if 0 <= row < len(matrix) and 0 <= col < len(matrix[0]) and not visited[row][col]:
+            unvisitedNeighbors.append([row, col])
+    return unvisitedNeighbors
