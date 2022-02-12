6502dd92ea9148c35adb78b0a754d9500e64ddc7
yingzhuo <yingzhuo1994@gmail.com>
2022-02-12 13:58:23 -0600

    2nd solution

--- CycleInGraph.py
+++ CycleInGraph.py
@@ -1,3 +1,6 @@
+# 1st solution
+# O(v + e) time | O(v) space
+# where v is the number of vertices and e is the nmber of edges in the graph
 def cycleInGraph(edges):
     numberOfNodes = len(edges)
     visited = [False for _ in range(numberOfNodes)]
@@ -27,4 +30,42 @@ def isNodeInCycle(node, edges, visited, currentlyInStack):
             return True
 
     currentlyInStack[node] = False
+    return False
+
+# 2nd solution
+# O(v + e) time | O(v) space
+# where v is the number of vertices and e is the nmber of edges in the graph
+WHITE, GREY, BLACK = 0, 1, 2
+def cycleInGraph(edges):
+    numberOfNodes = len(edges)
+    colors = [WHITE for _ in range(numberOfNodes)]
+
+    for node in range(numberOfNodes):
+        if colors[node] != WHITE:
+            continue
+
+        containsCycle = traverseAndColorNodes(node, edges, colors)
+        if containsCycle:
+            return True
+    
+    return False
+
+def traverseAndColorNodes(node, edges, colors):
+    colors[node] = GREY
+
+    neighbors = edges[node]
+    for neighbor in neighbors:
+        neighborColor = colors[neighbor]
+
+        if neighborColor == GREY:
+            return True
+        
+        if neighborColor == BLACK:
+            continue
+
+        containsCycle = traverseAndColorNodes(neighbor, edges, colors)
+        if containsCycle:
+            return True
+    
+    colors[node] = BLACK
     return False
