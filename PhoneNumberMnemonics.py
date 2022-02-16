1b52d0923b4834fe82f7298dbb8e99ea68d0e818
yingzhuo <yingzhuo1994@gmail.com>
2022-02-16 10:43:52 -0600

    renew

--- PhoneNumberMnemonics.py
+++ PhoneNumberMnemonics.py
@@ -1,5 +1,7 @@
+# 1st solution
+# O(4^n * n) time | O(4^n * n) space
+# where n is the length of the phone number
 def phoneNumberMnemonics(phoneNumber):
-    # Write your code here.
     dic = { '1': '1',
             '2': 'abc',
             '3': 'def',
@@ -16,4 +18,35 @@ def phoneNumberMnemonics(phoneNumber):
         lst = [s + ch for s in lst for ch in dic[num]]
     return lst
             
+# 2nd solution
+# O(4^n * n) time | O(4^n * n) space
+# where n is the length of the phone number
+def phoneNumberMnemonics(phoneNumber):
+    currentMnemonic = ["0"] * len(phoneNumber)
+    mnemonicsFound = []
+    phoneNumberMnemonicsHelper(0, phoneNumber, currentMnemonic, mnemonicsFound)
+    return mnemonicsFound
 
+def phoneNumberMnemonicsHelper(idx, phoneNumber, currentMnemonic, mnemonicsFound):
+    if idx == len(phoneNumber):
+        mnemonic = "".join(currentMnemonic)
+        mnemonicsFound.append(mnemonic)
+    else:
+        digit = phoneNumber[idx]
+        letters = DIGIT_LETTERS[digit]
+        for letter in letters:
+            currentMnemonic[idx] = letter
+            phoneNumberMnemonicsHelper(idx + 1, phoneNumber, currentMnemonic, mnemonicsFound)
+
+DIGIT_LETTERS = {
+    '1': '1',
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+    '0': '0'
+}
