2246d169fcefa5beb68d6fb6eb3fa8d9cc707611
yingzhuo <yingzhuo1994@gmail.com>
2022-02-19 10:56:14 -0600

    renew

--- SunsetViews.py
+++ SunsetViews.py
@@ -1,40 +1,49 @@
 # 1st solution
 # O(n) time | O(n) space
 def sunsetViews(buildings, direction):
-    lst = []
-    heightSoFar = 0
-    if direction == 'WEST':
-        for i, height in enumerate(buildings):
-            if height > heightSoFar:
-                lst.append(i)
-                heightSoFar = height
-        return lst
-    else:
-        for i in reversed(range(len(buildings))):
-            height = buildings[i]
-            if height > heightSoFar:
-                lst.append(i)
-                heightSoFar = height
-        return lst[::-1]
+    buildingsWithSunsetViews = []
 
-# 2nd solution
-# O(n) time | O(n) space
-def sunsetViews(buildings, direction):
-    lst = []
-    heightSoFar = 0
     if direction == 'WEST':
         idx = 0
         step = 1
     else:
         idx = len(buildings) - 1
         step = -1
-    
+
+    runningMaxHeight = 0
     while 0 <= idx < len(buildings):
-        height = buildings[idx]
-        if height > heightSoFar:
-            lst.append(idx)
-            heightSoFar = height
+        buildingHeight = buildings[idx]
+
+        if buildingHeight > runningMaxHeight:
+            buildingsWithSunsetViews.append(idx)
+            runningMaxHeight = buildingHeight
         idx += step
     if step < 0:
-        return lst[::-1]
-    return lst
+        return buildingsWithSunsetViews[::-1]
+    return buildingsWithSunsetViews
+
+# 2nd solution
+# O(n) time | O(n) space
+def sunsetViews(buildings, direction):
+    candidateBuildings = []
+
+    if direction == 'EAST':
+        idx = 0
+        step = 1
+    else:
+        idx = len(buildings) - 1
+        step = -1
+
+    while 0 <= idx < len(buildings):
+        buildingHeight = buildings[idx]
+        
+        while len(candidateBuildings) > 0 and buildings[candidateBuildings[-1]] <= buildingHeight:
+            candidateBuildings.pop()
+        
+        candidateBuildings.append(idx)
+        idx += step
+    
+    if step < 0:
+        return candidateBuildings[::-1]
+    
+    return candidateBuildings
