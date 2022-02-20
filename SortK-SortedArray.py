12a8840673b14ef45e716189b1410c356456ee77
yingzhuo <yingzhuo1994@gmail.com>
2022-02-19 18:21:25 -0600

    renew

--- SortK-SortedArray.py
+++ SortK-SortedArray.py
@@ -1,8 +1,8 @@
-import heapq
 # 1st solution
 # O(n*log(k)) time | O(k) space
 # where n is the nubmer of elements in the array
 # and k is how far away elements are from their sorted position
+import heapq
 def sortKSortedArray(array, k):
     if k > len(array):
         array.sort()
@@ -18,4 +18,30 @@ def sortKSortedArray(array, k):
         idx += 1
         array[i] = heapq.heappop(stack)
         i += 1
+    return array
+
+# 2nd solution
+# O(n*log(k)) time | O(k) space
+# where n is the nubmer of elements in the array
+# and k is how far away elements are from their sorted position
+import heapq
+def sortKSortedArray(array, k):
+    minHeapWithKElements = []
+    for i in range(min(k + 1, len(array))):
+        heapq.heappush(minHeapWithKElements, array[i])
+
+    nextIndexToInsertElement = 0
+    for idx in range(k + 1, len(array)):
+        minElement = heapq.heappop(minHeapWithKElements)
+        array[nextIndexToInsertElement] = minElement
+        nextIndexToInsertElement += 1
+
+        currentElement = array[idx]
+        heapq.heappush(minHeapWithKElements, currentElement)
+    
+    while len(minHeapWithKElements) > 0:
+        minElement = heapq.heappop(minHeapWithKElements)
+        array[nextIndexToInsertElement] = minElement
+        nextIndexToInsertElement += 1
+    
     return array
