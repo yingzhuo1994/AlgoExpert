604b8e8f1c5439d815a0a355ce343d0f67096c5b
yingzhuo <yingzhuo1994@gmail.com>
2022-02-27 11:01:51 -0600

    1st solution

--- MergeSort.py
+++ MergeSort.py
@@ -1,3 +1,37 @@
+# 1st solution
+# Best: O(nlog(n)) time | O(nlog(n)) space
+# Average: O(nlog(n)) time | O(nlog(n)) space
+# Worst: O(nlog(n)) time | O(nlog(n)) space
+def mergeSort(array):
+    if len(array) == 1:
+        return array
+    middleIdx = len(array) // 2
+    leftHalf = array[:middleIdx]
+    rightHalf = array[middleIdx:]
+    return mergeSortedArrays(mergeSort(leftHalf), mergeSort(rightHalf))
+
+def mergeSortedArrays(leftHalf, rightHalf):
+    sortedArray = []
+    i, j = 0, 0
+    while i < len(leftHalf) and j < len(rightHalf):
+        if leftHalf[i] <= rightHalf[j]:
+            sortedArray.append(leftHalf[i])
+            i += 1
+        else:
+            sortedArray.append(rightHalf[j])
+            j += 1
+    
+    while i < len(leftHalf):
+        sortedArray.append(leftHalf[i])
+        i += 1
+    
+    while j < len(rightHalf):
+        sortedArray.append(rightHalf[j])
+        j += 1
+    
+    return sortedArray
+
+# 2nd solution
 # Best: O(nlog(n)) time | O(n) space
 # Average: O(nlog(n)) time | O(n) space
 # Worst: O(nlog(n)) time | O(n) space
