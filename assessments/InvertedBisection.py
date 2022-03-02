27032dfa7da4de858f88b6119c4809040ff71a62
yingzhuo <yingzhuo1994@gmail.com>
2022-03-02 09:52:54 -0600

    2nd solution

--- assessments/InvertedBisection.py
+++ assessments/InvertedBisection.py
@@ -42,3 +42,57 @@ def reverseLinkedList(head):
         prev = node
         node = nextNode
     return prev
+
+# 2nd solution
+# O(n) time | o(1) space
+def invertedBisection(head):
+    length = getLinkedListLength(head)
+    if length <= 3:
+        return head
+    
+    firstHalfTail = getMiddleNode(head, length)
+    middleNode = None
+    secondHalfHead = None
+    if length % 2 == 0:
+        secondHalfHead = firstHalfTail.next
+    else:
+        middleNode = firstHalfTail.next
+        secondHalfHead = firstHalfTail.next.next
+    
+    firstHalfTail.next = None
+    reverseLinkedList(head)
+    reversedSecondHalfHead = reverseLinkedList(secondHalfHead)
+
+    if middleNode is None:
+        head.next = reversedSecondHalfHead
+    else:
+        head.next = middleNode
+        middleNode.next = reversedSecondHalfHead
+    
+    return firstHalfTail
+
+def getLinkedListLength(head):
+    length = 0
+    currentNode = head
+    while currentNode is not None:
+        currentNode = currentNode.next
+        length += 1
+    return length
+
+def getMiddleNode(head, length):
+    halfLength = length // 2
+    currentPosition = 1
+    currentNode = head
+    while currentPosition != halfLength:
+        currentNode = currentNode.next
+        currentPosition += 1
+    return currentNode
+
+def reverseLinkedList(head):
+    previousNode, currentNode = None, head
+    while currentNode is not None:
+        nextNode = currentNode.next
+        currentNode.next = previousNode
+        previousNode = currentNode
+        currentNode = nextNode
+    return previousNode
