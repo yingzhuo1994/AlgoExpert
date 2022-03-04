75175ec5c02418a0ded2fa983749907e5537aa33
yingzhuo <yingzhuo1994@gmail.com>
2022-03-03 21:11:39 -0600

    2nd solution

--- assessments/SpecialStrings.py
+++ assessments/SpecialStrings.py
@@ -26,3 +26,43 @@ def dfs(string, dic, ans, idx, count, cur):
             if dfs(string, dic, ans, i, count + 1, cur + partString):
                 return True
     return False
+
+# 2nd solution
+# Average case: when there aren't too many stings with identical prefixes
+# that are found in another string
+# O(n * s) time | O(n * s) space
+# where n is the number of strings and s is the length of the longest string
+def specialStrings(strings):
+    trie = Trie()
+    for string in strings:
+        trie.insert(string)
+    return list(filter(lambda string: isSpecial(string, trie.root, 0, 0, trie), strings))
+
+def isSpecial(string, trieNode, idx, count, trie):
+    char = string[idx]
+    if char not in trieNode:
+        return False
+    
+    atEndOfString = idx == len(string) - 1
+    if atEndOfString:
+        return trie.endSymbol in trieNode[char] and count + 1 >= 2
+    
+    if trie.endSymbol in trieNode[char]:
+        restIsSpecial = isSpecial(string, trie.root, idx + 1, count + 1, trie)
+        if restIsSpecial:
+            return True
+    
+    return isSpecial(string, trieNode[char], idx + 1, count, trie)
+
+class Trie:
+    def __init__(self):
+        self.root = {}
+        self.endSymbol = "*"
+    
+    def insert(self, string):
+        currentTrieNode = self.root
+        for i in range(len(string)):
+            if string[i] not in currentTrieNode:
+                currentTrieNode[string[i]] = {}
+            currentTrieNode = currentTrieNode[string[i]]
+        currentTrieNode[self.endSymbol] = string
