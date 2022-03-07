54d2e8943b01dbb08889046ae8e8612c33b4118c
yingzhuo <yingzhuo1994@gmail.com>
2022-03-07 10:35:29 -0600

    two official solutions

--- assessments/SpinRings.py
+++ assessments/SpinRings.py
@@ -21,3 +21,60 @@ def rotate(array, k):
     
     for i in reversed(range(start, end - 1)):
         array[i][start], tmp = tmp, array[i][start]
+
+# 2nd solution
+# O(n^2) time | O(n) space
+# where n is the size of the array
+def spinRings(array):
+    spinRingsHelper(array, 0, len(array) - 1, 0, len(array) - 1)
+
+def spinRingsHelper(array, startRow, endRow, startCol, endCol):
+    if startRow >= endRow and startCol >= endCol:
+        return 
+    
+    originalTopRightValue = array[startRow][endCol]
+
+    for col in reversed(range(startCol + 1, endCol + 1)):
+        array[startRow][col] = array[startRow][col - 1]
+    
+    for row in range(startRow, endRow):
+        array[row][startCol] = array[row + 1][startCol]
+    
+    for col in range(startCol, endCol):
+        array[endRow][col] = array[endRow][col + 1]
+    
+    for row in reversed(range(startRow + 2, endRow + 1)):
+        array[row][endCol] = array[row - 1][endCol]
+    
+    array[startRow + 1][endCol] = originalTopRightValue
+
+    spinRingsHelper(array, startRow + 1, endRow - 1, startCol + 1, endCol - 1)
+
+# 3rd solution
+# O(n^2) time | O(1) space
+# where n is the size of the array
+def spinRings(array):
+    startRow, endRow = 0, len(array) - 1
+    startCol, endCol = 0, len(array) - 1
+
+    while startRow < endRow and startCol < endCol:
+        originalTopRightValue = array[startRow][endCol]
+
+        for col in reversed(range(startCol + 1, endCol + 1)):
+            array[startRow][col] = array[startRow][col - 1]
+        
+        for row in range(startRow, endRow):
+            array[row][startCol] = array[row + 1][startCol]
+        
+        for col in range(startCol, endCol):
+            array[endRow][col] = array[endRow][col + 1]
+        
+        for row in reversed(range(startRow + 2, endRow + 1)):
+            array[row][endCol] = array[row - 1][endCol]
+        
+        array[startRow + 1][endCol] = originalTopRightValue
+
+        startRow += 1
+        endRow -= 1
+        startCol += 1
+        endCol -= 1
