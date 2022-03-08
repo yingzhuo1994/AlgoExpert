89767ee75ddccbca0b1a1af3f42e1736556eb32e
yingzhuo <yingzhuo1994@gmail.com>
2022-03-08 17:28:02 -0600

    3rd solution

--- assessments/BuildFailures.py
+++ assessments/BuildFailures.py
@@ -66,3 +66,46 @@ def getLongestDecreasingSubarrayLength(array):
             currentLongestLength = 1
     
     return longestLength if longestLength > 1 else -1
+
+# 3rd solution
+# O(n * log(m)) time | O(1) space
+# where n is the number of build runs
+# and m is the length of the longest build run
+def buildFailures(buildRuns):
+    longestLength = 1
+    currentLongestLength = 1
+    previousGreenPercentage = calculateGreenPercentage(buildRuns[0])
+
+    for i in range(1, len(buildRuns)):
+        currentGreenPercentage = calculateGreenPercentage(buildRuns[i])
+        if currentGreenPercentage < previousGreenPercentage:
+            currentLongestLength += 1
+            longestLength = max(longestLength, currentLongestLength)
+        else:
+            currentLongestLength = 1
+        previousGreenPercentage = currentGreenPercentage
+    
+    return longestLength if longestLength > 1 else -1
+
+def calculateGreenPercentage(buildRun):
+    firstFalseIdx = binarySearchForFirstFalse(buildRun)
+    return firstFalseIdx / len(buildRun)
+
+# Iterative Binary Search
+def binarySearchForFirstFalse(array):
+    leftIdx = 0
+    rightIdx = len(array) - 1
+
+    while leftIdx <= rightIdx:
+        middleIdx = (leftIdx + rightIdx) // 2
+        isFalse = not array[middleIdx]
+        if isFalse:
+            isFirstFalse = middleIdx == 0 or array[middleIdx - 1]
+            if isFirstFalse:
+                return middleIdx
+            else:
+                rightIdx = middleIdx - 1
+        else:
+            leftIdx = middleIdx + 1
+
+    return -1
