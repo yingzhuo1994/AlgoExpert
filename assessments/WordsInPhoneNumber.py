1f76741e5a139b71cc1d6f5ae266bc9c27ddcc82
yingzhuo <yingzhuo1994@gmail.com>
2022-03-08 13:10:11 -0600

    2nd solution

--- assessments/WordsInPhoneNumber.py
+++ assessments/WordsInPhoneNumber.py
@@ -1,4 +1,7 @@
 # 1st solution
+from typing import final
+
+
 def wordsInPhoneNumber(phoneNumber, words):
     trie = Trie()
     for i in range(len(phoneNumber)):
@@ -39,3 +42,62 @@ class Trie:
                 dic[ch] = {}
             dic = dic[ch]
         dic[self.end] = True
+
+# 2nd solution
+# O(m * w + n * 3^n) time | O(m * w + n) space
+# where n is the length of the phone number,
+# m is the number of words,
+# and w it the length of the longest word
+def wordsInPhoneNumber(phoneNumber, words):
+    wordTrie = getWordTrie(words)
+    finalWords = {}
+    for i in range(len(phoneNumber)):
+        exploreTrie(phoneNumber, i, wordTrie.root, finalWords)
+    return list(finalWords.keys())
+
+def getWordTrie(words):
+    wordTrie = Trie()
+    for word in words:
+        wordTrie.insert(word)
+    return wordTrie
+
+def exploreTrie(phoneNumber, digitIdx, trieNode, finalWords):
+    if "*" in trieNode:
+        word = trieNode["*"]
+        finalWords[word] = True
+    
+    if digitIdx == len(phoneNumber):
+        return 
+    
+    currentDigit = phoneNumber[digitIdx]
+    possibleLetters = DIGIT_LETTERS[currentDigit]
+    for letter in possibleLetters:
+        if letter not in trieNode:
+            continue
+        exploreTrie(phoneNumber, digitIdx + 1, trieNode[letter], finalWords)
+
+DIGIT_LETTERS = {
+    "0": [],
+    "1": [],
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"],
+}
+
+class Trie:
+    def __init__(self):
+        self.root = {}
+        self.endSymbol = "*"
+    
+    def insert(self, word):
+        currentTrieNode = self.root
+        for letter in word:
+            if letter not in currentTrieNode:
+                currentTrieNode[letter] = {}
+            currentTrieNode = currentTrieNode[letter]
+        currentTrieNode[self.endSymbol] = word
