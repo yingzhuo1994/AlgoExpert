fa9a2f2a9a7fae3ea4551e06eb9458faf70df3e3
yingzhuo <yingzhuo1994@gmail.com>
2022-03-08 16:33:10 -0600

    1st solution

---
+++ assessments/BalanceIndex.py
@@ -0,0 +1,19 @@
+# 1st solution
+# O(n) time | O(n) space
+def balanceIndex(array):
+    frontSumDic = {}
+    total = 0
+    for i in range(len(array)):
+        frontSumDic[i] = total
+        total += array[i]
+    
+    backSumDic = {}
+    total = 0
+    for i in reversed(range(len(array))):
+        backSumDic[i] = total
+        total += array[i]
+    
+    for i in range(len(array)):
+        if frontSumDic[i] == backSumDic[i]:
+            return i
+    return -1
