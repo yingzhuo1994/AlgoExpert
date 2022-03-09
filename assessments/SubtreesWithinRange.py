2dea84990191908f7aab92097339f41e5ae07935
yingzhuo <yingzhuo1994@gmail.com>
2022-03-08 20:51:29 -0600

    3rd solution

--- assessments/SubtreesWithinRange.py
+++ assessments/SubtreesWithinRange.py
@@ -78,3 +78,32 @@ class BST:
         self.left = None
         self.right = None
 
+# 3rd solution
+# Average case: when the tree is balanced
+# O(n) time | O(h) space
+# where n is the number of nodes in the BST and h is the height of the BST
+def subtreesWithinRange(tree, targetRange):
+    answer = {"result": 0}
+    isTreeWithinRange(tree, targetRange, answer)
+    return answer["result"]
+
+def isTreeWithinRange(tree, targetRange, answer):
+    if tree is None:
+        return True
+    
+    leftTreeWithinRange = isTreeWithinRange(tree.left, targetRange, answer)
+    rightTreeWithinRnage = isTreeWithinRange(tree.right, targetRange, answer)
+    nodeInRange = tree.value >= targetRange[0] and tree.value <= targetRange[1]
+    treeWithinRange = leftTreeWithinRange and rightTreeWithinRnage and nodeInRange
+
+    if treeWithinRange:
+        answer["result"] += 1
+    
+    return treeWithinRange
+
+# This is an input class. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
