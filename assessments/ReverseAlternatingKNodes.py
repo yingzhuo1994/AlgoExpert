72c2b6ea50f0ebdaec20c1c89fead9ca30ebbd09
yingzhuo <yingzhuo1994@gmail.com>
2022-03-09 11:45:16 -0600

    3rd solution

--- assessments/ReverseAlternatingKNodes.py
+++ assessments/ReverseAlternatingKNodes.py
@@ -34,3 +34,103 @@ def reverseAlternatingKNodes(head, k):
                 count += 1
             needReverse = True
     return sentinel.next
+
+# 2nd solution
+# O(n) time | O(1) space
+# where n is the number of nodes in the Linked List
+def reverseAlternatingKNodes(head, k):
+    finalHead = None
+    
+    isGroupToReverse = True
+    runningK = 1
+
+    previousGroupTail = None
+    currentGroupHead = head
+    currentNode = head
+
+    while currentNode is not None:
+        shouldReverse = isGroupToReverse and (runningK == k or currentNode.next is None)
+
+        if not shouldReverse:
+            if runningK == k:
+                runningK = 1
+                isGroupToReverse = True
+
+                previousGroupTail = currentNode
+                currentGroupHead = currentNode.next
+            else:
+                runningK += 1
+            
+            currentNode = currentNode.next
+            continue
+
+        runningK = 1
+        isGroupToReverse = False
+        
+        nextNode = currentNode.next
+        currentNode.next = None
+        reversedGroupHead = reverseLinkedList(currentGroupHead)
+        reversedGroupTail = currentGroupHead
+        reversedGroupTail.next = nextNode
+
+        if previousGroupTail is None:
+            finalHead = reversedGroupHead
+        else:
+            previousGroupTail.next = reversedGroupHead
+        
+        currentNode = nextNode
+        currentGroupHead = nextNode
+        previousGroupTail = reversedGroupTail
+    
+    return finalHead
+
+def reverseLinkedList(head):
+    previousNode, currentNode = None, head
+    while currentNode is not None:
+        nextNode = currentNode.next
+        currentNode.next = previousNode
+        previousNode = currentNode
+        currentNode = nextNode
+    return previousNode
+
+# 3rd solution
+# O(n) time | O(1) space
+# where n is the number of nodes in the Linked List
+def reverseAlternatingKNodes(head, k):
+    finalHead = None
+    
+    previousNode = None
+    currentNode = head
+    while currentNode is not None:
+        reversedGroupHead, nextNode = reverseKNodes(currentNode, k)
+        # The `currentNode` is now the tail of the reversed group,
+        # so we make it point to the `nextNode`
+        currentNode.next = nextNode
+        currentNode = nextNode
+
+        if previousNode is None:
+            finalHead = reversedGroupHead
+        else:
+            previousNode.next = reversedGroupHead
+        
+        skippedNodesCount = 0
+        while currentNode is not None and skippedNodesCount < k:
+            previousNode = currentNode
+            currentNode = currentNode.next
+
+            skippedNodesCount += 1
+    
+    return finalHead
+
+def reverseKNodes(head, k):
+    reversedNodesCount = 0
+    previousNode, currentNode = None, head
+    while currentNode is not None and reversedNodesCount < k:
+        nextNode = currentNode.next
+        currentNode.next = previousNode
+        previousNode = currentNode
+        currentNode = nextNode
+
+        reversedNodesCount += 1
+    
+    return (previousNode, currentNode)
