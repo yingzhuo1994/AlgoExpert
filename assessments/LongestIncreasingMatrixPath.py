52f2a09d6e23ec21f660024eb2701f84934ab77c
yingzhuo <yingzhuo1994@gmail.com>
2022-03-09 11:00:57 -0600

    2nd solution

--- assessments/LongestIncreasingMatrixPath.py
+++ assessments/LongestIncreasingMatrixPath.py
@@ -30,3 +30,43 @@ def getNeighbors(matrix, i, j):
         if 0 <= row and row < len(matrix) and 0 <= col and col < len(matrix[0]):
             neighbors.append([row, col])
     return neighbors
+
+# 2nd solution
+# O(n) time | O(n) space
+# where n is the total number of elements in the matrix
+def longestIncreasingMatrixPath(matrix):
+    longestPathLengths = []
+    for i in range(len(matrix)):
+        row = []
+        for j in range(len(matrix[0])):
+            row.append(None)
+        longestPathLengths.append(row)
+    
+    longestPathLength = 0
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            longestPathLength = max(
+                getLongestPathLengthAt(matrix, row, col, float("-inf"), longestPathLengths),
+                longestPathLength,
+            )
+    
+    return longestPathLength
+
+def getLongestPathLengthAt(matrix, row, col, lastPathValue, longestPathLengths):
+    isOutOfBounds = row < 0 or col < 0 or row >= len(matrix) or col >= len(matrix[0])
+    if isOutOfBounds:
+        return 0
+    
+    currentValue = matrix[row][col]
+    if currentValue <= lastPathValue:
+        return 0
+    
+    if longestPathLengths[row][col] is None:
+        longestPathLengths[row][col] = 1 + max(
+            getLongestPathLengthAt(matrix, row - 1, col, currentValue, longestPathLengths),
+            getLongestPathLengthAt(matrix, row + 1, col, currentValue, longestPathLengths),
+            getLongestPathLengthAt(matrix, row, col - 1, currentValue, longestPathLengths),
+            getLongestPathLengthAt(matrix, row, col + 1, currentValue, longestPathLengths)
+        )
+    
+    return longestPathLengths[row][col]
